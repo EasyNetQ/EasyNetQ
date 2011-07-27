dfd3231dfead82b608e34880fad7cffca17e4af5
Mike Hadlow <mike@suteki.co.uk>
2011-07-27 10:16:31 +0100

    Parsing arguments to EasyNetQ.Hosepipe

--- EasyNetQ.Hosepipe/Program.cs
+++ EasyNetQ.Hosepipe/Program.cs
@@ -1,20 +1,46 @@
 ﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Text;
 
 namespace EasyNetQ.Hosepipe
 {
     public class Program
     {
+        static readonly ArgParser argParser = new ArgParser();
+
         public static void Main(string[] args)
         {
-            foreach (var arg in args)
-            {
-                Console.WriteLine(arg);
-            }
+            var arguments = argParser.Parse(args);
+            PrintUsage();
         }
 
-        public static void GetMessagesFromQueue()
+        public static void PrintUsage()
         {
-            
+            using (var manifest = Assembly.GetExecutingAssembly().GetManifestResourceStream("EasyNetQ.Hosepipe.Usage.txt"))
+            {
+                if(manifest == null)
+                {
+                    throw new Exception("Could not load usage");
+                }
+
+//                var memoryStream = new MemoryStream();
+//                manifest.CopyTo(memoryStream);
+
+                var memoryStream = new MemoryStream();
+                var buffer = Encoding.UTF8.GetBytes("Hello from the memory stream");
+                memoryStream.Write(buffer, 0, buffer.Length);
+
+                Console.WriteLine("memoryStream.Length = {0}", memoryStream.Length);
+
+                using (var console = Console.OpenStandardOutput())
+                {
+                    // manifest.CopyTo(console);
+                    memoryStream.CopyTo(console);
+                    console.Position = 0;
+                    console.Flush();
+                }
+            }
         }
     }
 }
