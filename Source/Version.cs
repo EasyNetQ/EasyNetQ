dc70d091eb8d84ac3342c7f261dc3e3cea32db67
Mike Hadlow <mike.hadlow@15below.com>
2011-12-14 10:44:14 +0000

    build version number bump

--- Source/Version.cs
+++ Source/Version.cs
@@ -1,7 +1,7 @@
 using System.Reflection;
 
 // EasyNetQ verion number: <major>.<minor>.0.<build>
-[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyVersion("0.2.0.1")]
 
 // 0.2 Updgrade to RabbitMQ.Client 2.7.0.0
 // 0.1 Initial
