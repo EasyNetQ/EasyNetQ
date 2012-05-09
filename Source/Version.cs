23a4d95f795f273fe20094f9da5d9168a4148f4b
Mike Hadlow <mike.hadlow@15below.com>
2012-05-09 10:33:42 +0100

    Updated Version information

--- Source/Version.cs
+++ Source/Version.cs
@@ -3,7 +3,7 @@ using System.Reflection;
 // EasyNetQ version number: <major>.<minor>.0.<build>
 [assembly: AssemblyVersion("0.5.0.0")]
 
-// 0.5 Added IPublishChannel and moved Publish to it from IBus
+// 0.5 Added IPublishChannel and moved Publish and Request to it from IBus
 // 0.4 Topic based routing
 // 0.3 Upgrade to RabbitMQ.Client 2.8.1.0
 // 0.2 Upgrade to RabbitMQ.Client 2.7.0.0
