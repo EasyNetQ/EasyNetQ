905f862db3800cb96b59cd9a257291464f3d70ab
Mike Hadlow <mike.hadlow@15below.com>
2012-09-26 11:33:59 +0100

    Uppded version number for danielwertheim AutoSubscriber

--- Source/Version.cs
+++ Source/Version.cs
@@ -1,8 +1,9 @@
 ﻿using System.Reflection;
 
 // EasyNetQ version number: <major>.<minor>.0.<build>
-[assembly: AssemblyVersion("0.7.0.0")]
+[assembly: AssemblyVersion("0.7.1.0")]
 
+// 0.7.1.0  Daniel Wertheim's AutoSubscriber
 // 0.7.0.0  Added IServiceProvider to make it easy to plug in your own dependencies. Some breaking changes to RabbitHutch
 // 0.6.3.0  Consumer Queue now uses BCL BlockingCollection.
 // 0.6.2.0  New model cleanup strategy based on consumer tracking
