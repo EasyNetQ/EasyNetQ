f1b039648a4eaf96ce22a713669bd7826f016ef5
John-Mark Newton <jm.newton@15below.com>
2013-03-18 16:59:46 +0000

    Implement TPL style API for RPC requests on the publish tunnel

--- Source/EasyNetQ/IPublishChannel.cs
+++ Source/EasyNetQ/IPublishChannel.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 using EasyNetQ.FluentConfiguration;
 
 namespace EasyNetQ
@@ -43,6 +45,42 @@ namespace EasyNetQ
         /// <param name="onResponse">The action to run when the response is received.</param>
         /// <param name="arguments">AMQP arguments. For e.q. Message TTL("x-message-ttl", "60"), High Availability policy("x-ha-policy", "all") and so on.</param>
         void Request<TRequest, TResponse>(TRequest request, Action<TResponse> onResponse, IDictionary<string, object> arguments);
+
+        /// <summary>
+        /// Makes an RPC style request.
+        /// </summary>
+        /// <typeparam name="TRequest">The request type.</typeparam>
+        /// <typeparam name="TResponse">The response type.</typeparam>
+        /// <param name="request">The request message.</param>
+        Task<TResponse> RequestAsync<TRequest, TResponse>(TRequest request);
+
+        /// <summary>
+        /// Makes an RPC style request.
+        /// </summary>
+        /// <typeparam name="TRequest">The request type.</typeparam>
+        /// <typeparam name="TResponse">The response type.</typeparam>
+        /// <param name="request">The request message.</param>
+        /// <param name="arguments">AMQP arguments. For e.q. Message TTL("x-message-ttl", "60"), High Availability policy("x-ha-policy", "all") and so on.</param>
+        Task<TResponse> RequestAsync<TRequest, TResponse>(TRequest request, IDictionary<string, object> arguments);
+
+        /// <summary>
+        /// Makes an RPC style request.
+        /// </summary>
+        /// <typeparam name="TRequest">The request type.</typeparam>
+        /// <typeparam name="TResponse">The response type.</typeparam>
+        /// <param name="request">The request message.</param>
+        /// <param name="token">token that will cancel the RPC</param>
+        Task<TResponse> RequestAsync<TRequest, TResponse>(TRequest request, CancellationToken token);
+
+        /// <summary>
+        /// Makes an RPC style request.
+        /// </summary>
+        /// <typeparam name="TRequest">The request type.</typeparam>
+        /// <typeparam name="TResponse">The response type.</typeparam>
+        /// <param name="request">The request message.</param>
+        /// <param name="arguments">AMQP arguments. For e.q. Message TTL("x-message-ttl", "60"), High Availability policy("x-ha-policy", "all") and so on.</param>
+        /// <param name="token">token that will cancel the RPC</param>
+        Task<TResponse> RequestAsync<TRequest, TResponse>(TRequest request, IDictionary<string, object> arguments, CancellationToken token);
 
         /// <summary>
         /// The bus that created this channel
