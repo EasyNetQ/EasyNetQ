7ada72c8c57c65f65004ef8dfa368246f5c71fa2
Nick Van Eeckhout <nick.van.eeckhout@gmail.com>
2013-04-11 18:37:21 +0200

    Lets the user specify the routing key naming (and thus the queue naming) via conventions for request/response scenario. When not using the convention, default naming is typeserialization.

--- Source/EasyNetQ/Conventions.cs
+++ Source/EasyNetQ/Conventions.cs
@@ -5,7 +5,8 @@ namespace EasyNetQ
 {
 	public delegate string ExchangeNameConvention(Type messageType);
 	public delegate string TopicNameConvention(Type messageType);
-	public delegate string QueueNameConvention(Type messageType, string subscriberId);
+    public delegate string QueueNameConvention(Type messageType, string subscriberId);
+    public delegate string RpcRoutingKeyNamingConvention(Type messageType);
 
     public delegate string ErrorQueueNameConvention();
     public delegate string ErrorExchangeNameConvention(string  originalRoutingKey);
@@ -13,10 +14,10 @@ namespace EasyNetQ
 
 	public interface IConventions
 	{
-
 		ExchangeNameConvention ExchangeNamingConvention { get; set; }
 		TopicNameConvention TopicNamingConvention { get; set; }
-		QueueNameConvention QueueNamingConvention { get; set; }
+        QueueNameConvention QueueNamingConvention { get; set; }
+        RpcRoutingKeyNamingConvention RpcRoutingKeyNamingConvention { get; set; }
 
         ErrorQueueNameConvention ErrorQueueNamingConvention { get; set; }
         ErrorExchangeNameConvention ErrorExchangeNamingConvention { get; set; }
@@ -36,6 +37,7 @@ namespace EasyNetQ
 						var typeName = TypeNameSerializer.Serialize(messageType);
 						return string.Format("{0}_{1}", typeName, subscriptionId);
 					};
+            RpcRoutingKeyNamingConvention = TypeNameSerializer.Serialize;
 
             ErrorQueueNamingConvention = () => "EasyNetQ_Default_Error_Queue";
             ErrorExchangeNamingConvention = (originalRoutingKey) => "ErrorExchange_" + originalRoutingKey;
@@ -44,7 +46,8 @@ namespace EasyNetQ
 
 		public ExchangeNameConvention ExchangeNamingConvention { get; set; }
 		public TopicNameConvention TopicNamingConvention { get; set; }
-		public QueueNameConvention QueueNamingConvention { get; set; }
+        public QueueNameConvention QueueNamingConvention { get; set; }
+        public RpcRoutingKeyNamingConvention RpcRoutingKeyNamingConvention { get; set; }
 
         public ErrorQueueNameConvention ErrorQueueNamingConvention { get; set; }
         public ErrorExchangeNameConvention ErrorExchangeNamingConvention { get; set; }
