1e30616ec190bd352a46db705c5d4bf52d1fd3af
owen_thomson <owen.thomson@fridaymediagroup.com>
2013-05-10 08:23:54 +0100

    Hosepipe tool - Added arg support for max num msgs to fetch (n)

--- Source/EasyNetQ.Hosepipe/ArgParser.cs
+++ Source/EasyNetQ.Hosepipe/ArgParser.cs
@@ -59,6 +59,24 @@ namespace EasyNetQ.Hosepipe
             return TryResult.Pass();
         }
 
+        public TryResult WithKey<T>(string key, Action<Argument> argumentAction) where T:IConvertible
+        {
+            if (!keys.ContainsKey(key)) return TryResult.Fail();
+
+            try 
+            {
+                Convert.ChangeType(keys[key].Value, typeof(T));
+            } 
+            catch (InvalidCastException)
+            {
+                return TryResult.Fail();
+            }
+
+            var argument = keys[key];
+            argumentAction(argument);
+            return TryResult.Pass();
+        }
+
         public TryResult At(int position, string command, Action argumentAction)
         {
             if (position < 0 || position >= arguments.Count) return TryResult.Fail();
