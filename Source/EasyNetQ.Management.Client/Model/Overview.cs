b07c7ae4fc6b313f77f888f06c3be5a702fad70a
Fawad Halim <fawad@fawad.net>
2013-08-09 12:44:17 -0500

    Added capability to handle objects that are serialized by the rabbitmq REST API as an empty array when null. Fixed up some tests I've tried to get passing to be more portable.

--- Source/EasyNetQ.Management.Client/Model/Overview.cs
+++ Source/EasyNetQ.Management.Client/Model/Overview.cs
@@ -9,7 +9,7 @@ namespace EasyNetQ.Management.Client.Model
         public List<ExchangeType> ExchangeTypes { get; set; }
         public string RabbitmqVersion { get; set; }
         public string ErlangVersion { get; set; }
-        public List<object> MessageStats { get; set; }
+        public MessageStats MessageStats { get; set; }
         public QueueTotals QueueTotals { get; set; }
         public ObjectTotals ObjectTotals { get; set; }
         public string Node { get; set; }
