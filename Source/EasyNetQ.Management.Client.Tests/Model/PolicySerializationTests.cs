8771d7f0be8adb518f509bf45870fe8b27e84188
Fawad Halim <fawad@fawad.net>
2013-08-13 16:01:57 -0500

    Initial work to model HA Policies.

---
+++ Source/EasyNetQ.Management.Client.Tests/Model/PolicySerializationTests.cs
@@ -0,0 +1,28 @@
+﻿namespace EasyNetQ.Management.Client.Tests.Model
+{
+    using System.Linq;
+    using Client.Model;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class PolicySerializationTests
+    {
+        private Policy[] _policy;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _policy = ResourceLoader.LoadObjectFromJson<Policy[]>("Policies_ha_exactly.json", ManagementClient.Settings);
+        }
+
+        [Test]
+        public void Should_read_exactly_ha_properly()
+        {
+            _policy.Count().ShouldEqual(1);
+            var firstPolicy = _policy.First();
+            firstPolicy.Definition.ShouldNotBeNull();
+            firstPolicy.Definition.HaMode.ShouldEqual(HaMode.Exactly);
+            firstPolicy.Definition.HaParams.ExactlyCount.ShouldEqual(2);
+        }
+    }
+}
---
+++ Source/EasyNetQ.Management.Client/Model/Policy.cs
@@ -0,0 +1,40 @@
+﻿namespace EasyNetQ.Management.Client.Model
+{
+    public class Policy
+    {
+        public string Vhost { get; set; }
+        public string Name { get; set; }
+        public string Pattern { get; set; }
+        public PolicyDefinition Definition { get; set; }
+        public int Priority { get; set; }
+    }
+
+    public class PolicyDefinition
+    {
+        public HaMode HaMode;
+        public HaParams HaParams;
+        public HaSyncMode HaSyncMode;
+        public string FederationUpstreamSet { get; set; }
+    }
+
+    public class HaParams
+    {
+        public int ExactlyCount { get; set; }
+        public string[] Nodes { get; set; }
+    }
+
+    public enum HaSyncMode
+    {
+        Unset,
+        Manual,
+        Automatic
+    }
+
+    public enum HaMode
+    {
+        Unset,
+        All,
+        Exactly,
+        Nodes
+    }
+}
