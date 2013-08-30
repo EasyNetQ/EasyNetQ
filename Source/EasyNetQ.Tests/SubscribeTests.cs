65a847d37f6480aea614251fade619a27b67afd1
Mike Hadlow <mike@suteki.co.uk>
2013-08-30 11:30:11 +0100

    SubscribeTests better factored.

--- Source/EasyNetQ.Tests/SubscribeTests.cs
+++ Source/EasyNetQ.Tests/SubscribeTests.cs
@@ -22,10 +22,6 @@ namespace EasyNetQ.Tests
         private const string queueName = typeName + "_" + subscriptionId;
         private const string consumerTag = "the_consumer_tag";
 
-        private readonly ConnectionConfiguration connectionConfiguration = new ConnectionConfiguration();
-
-        private MyMessage deliveredMessage;
-
         [SetUp]
         public void SetUp()
         {
@@ -39,10 +35,7 @@ namespace EasyNetQ.Tests
                 //.Register<IEasyNetQLogger>(_ => new ConsoleLogger())
                 );
 
-            mockBuilder.Bus.Subscribe<MyMessage>(subscriptionId, message =>
-                {
-                    deliveredMessage = message;
-                });
+            mockBuilder.Bus.Subscribe<MyMessage>(subscriptionId, message => { });
         }
 
         [Test]
@@ -79,6 +72,7 @@ namespace EasyNetQ.Tests
         [Test]
         public void Should_set_configured_prefetch_count()
         {
+            var connectionConfiguration = new ConnectionConfiguration();
             mockBuilder.Channels[0].AssertWasCalled(x => x.BasicQos(0, connectionConfiguration.PrefetchCount, false));
         }
 
@@ -98,36 +92,76 @@ namespace EasyNetQ.Tests
         {
             mockBuilder.Consumers.Count.ShouldEqual(1);
         }
+    }
+
+    [TestFixture]
+    public class When_a_message_is_delivered
+    {
+        private MockBuilder mockBuilder;
+
+        private const string typeName = "EasyNetQ_Tests_MyMessage:EasyNetQ_Tests";
+        private const string subscriptionId = "the_subscription_id";
+        private const string consumerTag = "the_consumer_tag";
+        private const ulong deliveryTag = 123;
+
+        private MyMessage originalMessage;
+        private MyMessage deliveredMessage;
+
+        [SetUp]
+        public void SetUp()
+        {
+            var conventions = new Conventions
+            {
+                ConsumerTagConvention = () => consumerTag
+            };
+
+            mockBuilder = new MockBuilder(x => x
+                .Register<IConventions>(_ => conventions)
+                //.Register<IEasyNetQLogger>(_ => new ConsoleLogger())
+                );
+
+            var autoResetEvent = new AutoResetEvent(false);
+            mockBuilder.Bus.Subscribe<MyMessage>(subscriptionId, message =>
+            {
+                deliveredMessage = message;
+                autoResetEvent.Set();
+            });
+
+
+            const string text = "Hello there, I am the text!";
+            originalMessage = new MyMessage { Text = text };
+
+            var body = new JsonSerializer().MessageToBytes(originalMessage);
+
+            // deliver a message
+            mockBuilder.Consumers[0].HandleBasicDeliver(
+                consumerTag,
+                deliveryTag,
+                false, // redelivered
+                typeName,
+                "#",
+                new BasicProperties
+                {
+                    Type = typeName,
+                    CorrelationId = "some correlation id"
+                },
+                body);
+
+            // wait for the subscription thread to handle the message ...
+            autoResetEvent.WaitOne(1000);
+        }
 
         [Test]
         public void Should_deliver_message()
         {
-            var properties = new BasicProperties
-                {
-                    Type = typeName,
-                    CorrelationId = "some correlation id"
-                };
-
-            const string text = "Hello there, I am the text!";
-            var originalMessage = new MyMessage {Text = text};
-            var serializer = new JsonSerializer();
-
-            var body = serializer.MessageToBytes(originalMessage);
-
-            var consumer = mockBuilder.Consumers[0];
-            consumer.HandleBasicDeliver(
-                consumerTag,
-                0,
-                false, // redelivered
-                typeName,
-                "#",
-                properties,
-                body);
-
-            Thread.Sleep(100);
-
             deliveredMessage.ShouldNotBeNull();
-            deliveredMessage.Text.ShouldEqual(text);
+            deliveredMessage.Text.ShouldEqual(originalMessage.Text);
+        }
+
+        [Test]
+        public void Should_ack_the_message()
+        {
+            mockBuilder.Channels[0].AssertWasCalled(x => x.BasicAck(deliveryTag, false));
         }
     }
 }
