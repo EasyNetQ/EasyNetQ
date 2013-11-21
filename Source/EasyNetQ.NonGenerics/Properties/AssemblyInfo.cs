be817e6cd37622d4795aa657dac72ae3dcfebbdf
Rafael Romão <rafael.romao@concert.com.br>
2013-11-21 08:34:57 -0200

    Created EasyNetQ.NonGenerics
    EasyNetQ.NonGenerics contains extension methods for PublishAsync and
    Subscribe that resolve the message type at runtime, without using
    generic parameters.

---
+++ Source/EasyNetQ.NonGenerics/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("EasyNetQ.NonGenerics")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EasyNetQ.NonGenerics")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("80fd3b99-dc16-4606-be6a-1f00595c9dfc")]
---
+++ Source/EasyNetQ.Tests.NonGenerics/EasyNetQNonGenericsExtensionsTests.cs
@@ -0,0 +1,192 @@
+﻿using EasyNetQ;
+using EasyNetQ.ConnectionString;
+using EasyNetQ.NonGenerics;
+using EasyNetQ.Topology;
+using FluentAssertions;
+using Logger.Infrastructure;
+using NUnit.Framework;
+using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EasyNetQ.Tests.NonGenerics
+{
+    [TestFixture]
+    public class EasyNetQNonGenericsExtensionsTests
+    {
+        internal static string ConnectionString = "host=192.168.4.5";
+
+        public class MyMessage
+        {
+            public Guid Id { get; set; }
+        }
+
+        private IBus NewBus(bool publisherConfirms)
+        {
+            var connectionStringParser = new ConnectionStringParser();
+            var connectionConfiguration = connectionStringParser.Parse(ConnectionString) as ConnectionConfiguration;
+            connectionConfiguration.PublisherConfirms = publisherConfirms;
+
+            return RabbitHutch.CreateBus(connectionConfiguration, s => { });
+        }
+
+        [Test]
+        [TestCase("Generics")]
+        [TestCase("NonGenerics")]
+        public void TestEasyNetQ(string messageTypeIdentificationMode)
+        {
+            var runtime = 1000;
+            var receivedMessageCount = 0;
+            var publishedMessageCount = 0;
+            var requirePublishConfirmation = true;
+            var subscriptionId = "TestEasyNetQ";
+
+            using (var subscriptionBus = NewBus(requirePublishConfirmation))
+            {
+                try
+                {
+                    switch (messageTypeIdentificationMode)
+                    {
+                        case "Generics":
+                            subscriptionBus.Subscribe<MyMessage>(
+                                subscriptionId,
+                                m =>
+                                {
+                                    receivedMessageCount++;
+                                }
+                            );
+                            break;
+                        case "NonGenerics":
+                            subscriptionBus.Subscribe(
+                                typeof(MyMessage),
+                                subscriptionId,
+                                m =>
+                                {
+                                    receivedMessageCount++;
+                                }
+                            );
+                            break;
+                    }
+
+                    using (var publishBus = NewBus(requirePublishConfirmation))
+                    {
+                        var stopwatch = new Stopwatch();
+                        stopwatch.Start();
+                        while (stopwatch.ElapsedMilliseconds < runtime)
+                        {
+                            Task publishingTask = null;
+
+                            var message = new MyMessage { Id = Guid.NewGuid() };
+                            switch (messageTypeIdentificationMode)
+                            {
+                                case "Generics":
+                                    publishingTask = publishBus.PublishAsync<MyMessage>(message);
+                                    break;
+                                case "NonGenerics":
+                                    publishingTask = publishBus.PublishAsync(null, message);
+                                    break;
+                            }
+
+                            if (requirePublishConfirmation)
+                                publishingTask.Wait();
+
+                            publishedMessageCount++;
+                        }
+                        stopwatch.Stop();
+                    }
+                }
+                finally
+                {
+                    subscriptionBus.Advanced.QueueDelete(new Queue(subscriptionBus.QueueNameFor(typeof(MyMessage), subscriptionId), false));
+                }
+            }
+
+            // Assert all messages sent were received
+            publishedMessageCount.Should().Be(receivedMessageCount);
+        }
+
+        [Test]
+        [TestCase("WithPublishConfirms", "Generics")]
+        [TestCase("", "Generics")]
+        [TestCase("WithPublishConfirms", "NonGenerics")]
+        [TestCase("", "NonGenerics")]
+        public void TestEasyNetQTransferRate(string publishConfirmationMode, string messageTypeIdentificationMode)
+        {
+            var runtime = 1000;
+            var receivedMessageCount = 0;
+            var publishedMessageCount = 0;
+            var requirePublishConfirmation = publishConfirmationMode == "WithPublishConfirms";
+            var subscriptionId = "TestEasyNetQTransferRate";
+
+            using (var subscriptionBus = NewBus(requirePublishConfirmation))
+            {
+                try
+                {
+                    switch (messageTypeIdentificationMode)
+                    {
+                        case "Generics":
+                            subscriptionBus.Subscribe<MyMessage>(
+                                subscriptionId,
+                                m =>
+                                {
+                                    receivedMessageCount++;
+                                }
+                            );
+                            break;
+                        case "NonGenerics":
+                            subscriptionBus.Subscribe(
+                                typeof(MyMessage),
+                                subscriptionId,
+                                m =>
+                                {
+                                    receivedMessageCount++;
+                                }
+                            );
+                            break;
+                    }
+
+                    using (var publishBus = NewBus(requirePublishConfirmation))
+                    {
+                        var stopwatch = new Stopwatch();
+                        stopwatch.Start();
+                        while (stopwatch.ElapsedMilliseconds < runtime)
+                        {
+                            Task publishingTask = null;
+
+                            var message = new MyMessage { Id = Guid.NewGuid() };
+                            switch (messageTypeIdentificationMode)
+                            {
+                                case "Generics":
+                                    publishingTask = publishBus.PublishAsync<MyMessage>(message);
+                                    break;
+                                case "NonGenerics":
+                                    publishingTask = publishBus.PublishAsync(null, message);
+                                    break;
+                            }
+
+                            if (requirePublishConfirmation)
+                                publishingTask.Wait();
+
+                            publishedMessageCount++;
+                        }
+                        stopwatch.Stop();
+                    }
+                }
+                finally
+                {
+                    subscriptionBus.Advanced.QueueDelete(new Queue(subscriptionBus.QueueNameFor(typeof(MyMessage), subscriptionId), false));
+                }
+            }
+
+            Console.WriteLine(String.Format("Published: {0}, Received: {1}, Run Time: {2}", publishedMessageCount, receivedMessageCount, runtime));
+
+            if (requirePublishConfirmation)
+            {
+                receivedMessageCount.Should().Be(publishedMessageCount);
+            }
+        }
+    }
+}
