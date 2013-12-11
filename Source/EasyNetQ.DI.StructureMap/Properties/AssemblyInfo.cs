54a780fe16003868039beb52bc9909461824acb9
Jeff Doolittle <jeffdoolittle@gmail.com>
2013-12-11 15:27:53 -0800

    Support for StructureMap, Windsor and Ninject container factory adapters

---
+++ Source/EasyNetQ.DI.StructureMap/Properties/AssemblyInfo.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("EasyNetQ.DI.StructureMap")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("EasyNetQ")]
+[assembly: AssemblyProduct("EasyNetQ.DI.StructureMap")]
+[assembly: AssemblyCopyright("Copyright © EasyNetQ 2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7233ff8e-5b46-4133-a2c3-de69cf51b0a5")]
