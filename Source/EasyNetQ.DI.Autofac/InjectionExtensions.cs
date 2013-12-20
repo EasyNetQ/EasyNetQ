dd22b2483e96834bdc00d1490a30fb2916cecd00
Wiebe Tijsma <wiebe@tijsma.com>
2013-12-20 12:03:56 +0100

    Added Autofac support for internal DI

---
+++ Source/EasyNetQ.DI.Autofac/InjectionExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Autofac;
+
+namespace EasyNetQ.DI
+{
+    public static class InjectionExtensions
+    {
+        public static void RegisterAsEasyNetQContainerFactory(this ContainerBuilder builder)
+        {
+            var autofacAdapter = new AutofacAdapter(builder);
+            RabbitHutch.SetContainerFactory(() => autofacAdapter);
+        }
+    }
+}
---
+++ Source/EasyNetQ.DI.Tests/AutofacAdapterTests.cs
@@ -0,0 +1,47 @@
+﻿using Autofac;
+using EasyNetQ.Tests.Mocking;
+using NUnit.Framework;
+
+namespace EasyNetQ.DI.Tests
+{
+    /// <summary>
+    ///     EasyNetQ expects that the DI container will follow a first-to-register-wins
+    ///     policy. The internal DefaultServiceProvider works this way, as does Windsor.
+    ///     However, Ninject doesn't allow more than one registration of a service, it
+    ///     throws an exception, and StructureMap and Autofac have a last-to-register-wins policy.
+    /// </summary>
+    [TestFixture]
+    [Explicit("Starts a connection to localhost")]
+    public class AutofacAdapterTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            builder = new ContainerBuilder();
+            builder.Register(c => new MockBuilder().Bus).As<IBus>();
+            autofacAdapter = new AutofacAdapter(builder);
+
+            RabbitHutch.SetContainerFactory(() => autofacAdapter);
+            container = autofacAdapter.Container;
+            bus = container.Resolve<IBus>();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            container.Dispose();
+            RabbitHutch.SetContainerFactory(() => new DefaultServiceProvider());
+        }
+
+        private ContainerBuilder builder;
+        private IBus bus;
+        private Autofac.IContainer container;
+        private AutofacAdapter autofacAdapter;
+
+        [Test]
+        public void Should_create_bus_with_autofac_module()
+        {
+            Assert.IsNotNull(bus);
+        }
+    }
+}
