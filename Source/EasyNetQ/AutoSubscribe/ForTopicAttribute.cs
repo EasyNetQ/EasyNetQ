bdcbb49eae74e33c92024ff61763233c2321f3bb
John Efford <johneffo@gmail.com>
2014-01-13 11:33:39 +0000

    Add ForTopic Attribute which allows the specification of topics for AutoSubscriber

---
+++ Source/EasyNetQ/AutoSubscribe/ForTopicAttribute.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace EasyNetQ.AutoSubscribe
+{
+    [Serializable]
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
+    public class ForTopicAttribute : Attribute
+    {
+        public ForTopicAttribute(string topic)
+        {
+            Topic = topic;
+        }
+
+        public string Topic { get; set; }
+    }
+}
