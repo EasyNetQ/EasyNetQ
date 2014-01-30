848db6e770a861ae66e96a5ce7dd329e8b80195f
wtijsma <wiebe@tijsma.com>
2014-01-31 00:20:33 +0100

    Added options Durable(bool) and AutoDelete(bool) options to the ISubscriptionConfiguration and added an optional Action<ISubscriptionConfiguration> property to AutoSubscriber to be able to set this configuration as a default for a bus instance

--- Source/EasyNetQ/FluentConfiguration/ISubscriptionConfiguration.cs
+++ Source/EasyNetQ/FluentConfiguration/ISubscriptionConfiguration.cs
@@ -17,20 +17,51 @@ namespace EasyNetQ.FluentConfiguration
         /// <param name="topic">The topic to add</param>
         /// <returns></returns>
         ISubscriptionConfiguration WithTopic(string topic);
+        
+        /// <summary>
+        /// Indicate if queues for this subscription are declared durable (default:true)
+        /// </summary>
+        ISubscriptionConfiguration Durable(bool durable);
+
+        /// <summary>
+        /// Automatically delete queues after disconnect
+        /// </summary>
+        ISubscriptionConfiguration AutoDelete(bool autoDelete);
     }
 
     public class SubscriptionConfiguration : ISubscriptionConfiguration
     {
         public IList<string> Topics { get; private set; }
 
+        public bool Durable { get; set; }
+
+        public bool HighAvailable { get; set; }
+        
+        public bool AutoDelete { get; set; }
+
         public SubscriptionConfiguration()
         {
             Topics = new List<string>();
+            Durable = true;
+            HighAvailable = false;
+            AutoDelete = false;
         }
 
-        public ISubscriptionConfiguration WithTopic(string topic)
+        ISubscriptionConfiguration ISubscriptionConfiguration.WithTopic(string topic)
         {
             Topics.Add(topic);
+            return this;
+        }
+
+        ISubscriptionConfiguration ISubscriptionConfiguration.Durable(bool durable)
+        {
+            Durable = durable;
+            return this;
+        }
+
+        ISubscriptionConfiguration ISubscriptionConfiguration.AutoDelete(bool autoDelete)
+        {
+            AutoDelete = autoDelete;
             return this;
         }
     }
