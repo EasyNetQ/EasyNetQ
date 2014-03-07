f12f602228ba4f406ad2fd4885e75f71903d0c87
Yury Pliner <yury.pliner@gmail.com>
2014-03-07 18:15:38 +0600

    FuturePublishAsync

--- Source/EasyNetQ/BusExtensions.cs
+++ Source/EasyNetQ/BusExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 using EasyNetQ.SystemMessages;
 
 namespace EasyNetQ
@@ -26,6 +27,35 @@ namespace EasyNetQ
             var messageBody = serializer.MessageToBytes(message);
 
             bus.Publish(new ScheduleMe
+            {
+                WakeTime = futurePublishDate,
+                BindingKey = typeName,
+                InnerMessage = messageBody
+            });
+        }
+
+
+        /// <summary>
+        /// Schedule a message to be published at some time in the future.
+        /// This required the EasyNetQ.Scheduler service to be running.
+        /// </summary>
+        /// <typeparam name="T">The message type</typeparam>
+        /// <param name="bus">The IBus instance to publish on</param>
+        /// <param name="futurePublishDate">The time at which the message should be sent (UTC)</param>
+        /// <param name="message">The message to response with</param>
+        public static Task FuturePublishAsync<T>(this IBus bus, DateTime futurePublishDate, T message) where T : class
+        {
+            Preconditions.CheckNotNull(message, "message");
+
+            var advancedBus = bus.Advanced;
+
+            var typeNameSerializer = advancedBus.Container.Resolve<ITypeNameSerializer>();
+            var serializer = advancedBus.Container.Resolve<ISerializer>();
+
+            var typeName = typeNameSerializer.Serialize(typeof(T));
+            var messageBody = serializer.MessageToBytes(message);
+
+            return bus.PublishAsync(new ScheduleMe
             {
                 WakeTime = futurePublishDate,
                 BindingKey = typeName,
