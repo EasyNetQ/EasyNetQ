dd2d40ab06cac8549880babcf78ae5093037420f
Robert Hager <robert.hager@asynchrony.com>
2014-04-14 09:44:22 -0500

    move classes to separate files

---
+++ Source/EasyNetQ/Producer/PublisherBase.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading.Tasks;
+using EasyNetQ.Events;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace EasyNetQ.Producer
+{
+    public abstract class PublisherBase : IPublisher
+    {
+        private readonly IEventBus eventBus;
+        private IModel cachedModel;
+
+        protected PublisherBase(IEventBus eventBus)
+        {
+            Preconditions.CheckNotNull(eventBus, "eventBus");
+            
+            this.eventBus = eventBus;
+        }
+
+        protected void SetModel(IModel model)
+        {
+            // we only need to set up the channel once, but the persistent channel can change
+            // the IModel instance underneath us, so check on each publish.
+            if (cachedModel == model) return;
+
+            if (cachedModel != null)
+            {
+                OnChannelClosed(cachedModel);
+            }
+
+            cachedModel = model;
+
+            OnChannelOpened(model);
+        }
+
+        protected virtual void OnChannelOpened(IModel newModel)
+        {
+            newModel.BasicReturn += ModelOnBasicReturn;
+        }
+
+        protected virtual void OnChannelClosed(IModel oldModel)
+        {
+            oldModel.BasicReturn -= ModelOnBasicReturn;
+        }
+
+        public abstract Task Publish(IModel model, Action<IModel> publishAction);
+
+        protected void ModelOnBasicReturn(IModel model, BasicReturnEventArgs args)
+        {
+            eventBus.Publish(new ReturnedMessageEvent(args.Body,
+                                                      new MessageProperties(args.BasicProperties),
+                                                      new MessageReturnedInfo(args.Exchange, args.RoutingKey, args.ReplyText)));
+        }
+
+        protected struct NullStruct { }
+    }
+}
