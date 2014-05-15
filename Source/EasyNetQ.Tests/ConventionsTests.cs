08e1de858898989bbebf3cdf7ebfc2193d62bdfa
Mariusz Wojcik <mariusz.wojcik@hotmail.co.uk>
2014-05-15 21:49:29 +0100

    Added QueueAttribute for controling queue / exchange name.

--- Source/EasyNetQ.Tests/ConventionsTests.cs
+++ Source/EasyNetQ.Tests/ConventionsTests.cs
@@ -76,6 +76,58 @@ namespace EasyNetQ.Tests
         }
 	}
 
+    [TestFixture]
+    public class When_using_QueueAttribute
+    {
+        private Conventions conventions;
+        private ITypeNameSerializer typeNameSerializer;
+
+        [SetUp]
+        public void SetUp()
+        {
+            typeNameSerializer = new TypeNameSerializer();
+            conventions = new Conventions(typeNameSerializer);
+        }
+
+        [Test]
+        public void The_queue_naming_convention_should_use_attribute_queueName_then_an_underscore_then_the_subscription_id()
+        {
+            const string subscriptionId = "test";
+            var result = conventions.QueueNamingConvention(typeof(AnnotatedTestMessage), subscriptionId);
+            result.ShouldEqual("MyQueue" + "_" + subscriptionId);
+        }
+
+        [Test]
+        public void And_subscription_id_is_empty_the_queue_naming_convention_should_use_attribute_queueName()
+        {
+            const string subscriptionId = "";
+            var result = conventions.QueueNamingConvention(typeof(AnnotatedTestMessage), subscriptionId);
+            result.ShouldEqual("MyQueue");
+        }
+
+        [Test]
+        public void And_queueName_is_empty_should_use_the_TypeNameSerializers_Serialize_method_then_an_underscore_then_the_subscription_id()
+        {
+            const string subscriptionId = "test";
+            var result = conventions.QueueNamingConvention(typeof(EmptyQueueNameAnnotatedTestMessage), subscriptionId);
+            result.ShouldEqual(typeNameSerializer.Serialize(typeof(EmptyQueueNameAnnotatedTestMessage)) + "_" + subscriptionId);
+        }
+
+        [Test]
+        public void The_exchange_name_convention_should_use_attribute_exchangeName()
+        {
+            var result = conventions.ExchangeNamingConvention(typeof(AnnotatedTestMessage));
+            result.ShouldEqual("MyExchange");
+        }
+
+        [Test]
+        public void And_exchangeName_not_specified_the_exchange_name_convention_should_use_the_TypeNameSerializers_Serialize_method()
+        {
+            var result = conventions.ExchangeNamingConvention(typeof(QueueNameOnlyAnnotatedTestMessage));
+            result.ShouldEqual(typeNameSerializer.Serialize(typeof(QueueNameOnlyAnnotatedTestMessage)));
+        }
+    }
+
 	[TestFixture]
 	public class When_publishing_a_message
 	{
---
+++ Source/EasyNetQ/QueueAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EasyNetQ
+{
+    [Serializable]
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple=false)]
+    public class QueueAttribute : Attribute
+    {
+        public QueueAttribute(string queueName)
+        {
+            QueueName = queueName ?? string.Empty;
+        }
+
+        public string QueueName { get; private set; }
+        public string ExchangeName { get; set; }
+    }
+}
