8142e435d2b0564a3fa5b2ed5ce0bcee9b84c51c
Michael Denny <micdenny@gmail.com>
2014-07-01 17:33:38 +0200

    x-expires now can be configured while subscribe, using the fluent interface method x => x.WithExpires(int)

---
+++ Source/EasyNetQ.Tests/Integration/SubscribeWithExpiresTests.cs
@@ -0,0 +1,67 @@
+﻿using System.Linq;
+using EasyNetQ.Management.Client;
+using EasyNetQ.Management.Client.Model;
+// ReSharper disable InconsistentNaming
+using System;
+using System.Threading;
+using NUnit.Framework;
+
+namespace EasyNetQ.Tests.Integration
+{
+    [TestFixture]
+    public class SubscribeWithExpiresTests
+    {
+        [Test, Explicit("Needs a Rabbit instance on localhost to work")]
+        [ExpectedException(typeof(UnexpectedHttpStatusCodeException))]
+        public void Queue_should_be_deleted_after_the_expires_ttl()
+        {
+            var bus = RabbitHutch.CreateBus("host=localhost");
+
+            var subscriptionId = "TestSubscriptionWithExpires";
+            var conventions = new Conventions(new TypeNameSerializer());
+            var queueName = conventions.QueueNamingConvention(typeof(MyMessage), subscriptionId);
+            var client = new ManagementClient("http://localhost", "guest", "guest");
+            var vhost = new Vhost { Name = "/" };
+
+            bus.Subscribe<MyMessage>(subscriptionId, message => { }, x => x.WithExpires(1000));
+
+            var queue = client.GetQueue(queueName, vhost);
+            queue.ShouldNotBeNull();
+            
+            // this will abandon the queue... poor queue!
+            bus.Dispose();
+
+            Thread.Sleep(1500);
+
+            queue = client.GetQueue(queueName, vhost);
+            queue.ShouldBeNull();
+        }
+
+        [Test, Explicit("Needs a Rabbit instance on localhost to work")]
+        public void Queue_should_not_be_deleted_if_expires_is_not_set()
+        {
+            var bus = RabbitHutch.CreateBus("host=localhost");
+
+            var subscriptionId = "TestSubscriptionWithoutExpires";
+            var conventions = new Conventions(new TypeNameSerializer());
+            var queueName = conventions.QueueNamingConvention(typeof(MyMessage), subscriptionId);
+            var client = new ManagementClient("http://localhost", "guest", "guest");
+            var vhost = new Vhost { Name = "/" };
+
+            bus.Subscribe<MyMessage>(subscriptionId, message => { });
+
+            var queue = client.GetQueue(queueName, vhost);
+            queue.ShouldNotBeNull();
+
+            // this will abandon the queue... poor queue!
+            bus.Dispose();
+
+            Thread.Sleep(1500);
+
+            queue = client.GetQueue(queueName, vhost);
+            queue.ShouldNotBeNull();
+        }
+    }
+}
+
+// ReSharper restore InconsistentNaming
