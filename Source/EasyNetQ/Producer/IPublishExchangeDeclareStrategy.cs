100485c77fab7d07c81732ee9e15fb9592510a7d
Tordenhunden <steffen.daniel.jensen@gmail.com>
2014-07-31 15:31:29 +0200

    1. Split IPublishExchangeDeclareStrategy in two..   * PublishExchangeDeclareStrategy was referring directly to the container. Moved to constructor   * Split interface in two: An advanced and a convention based one.

--- Source/EasyNetQ/Producer/IPublishExchangeDeclareStrategy.cs
+++ Source/EasyNetQ/Producer/IPublishExchangeDeclareStrategy.cs
@@ -6,9 +6,13 @@ namespace EasyNetQ.Producer
 {
     public interface IPublishExchangeDeclareStrategy
     {
-        IExchange DeclareExchange(IAdvancedBus advancedBus, string exchangeName, string exchangeType);
-        IExchange DeclareExchange(IAdvancedBus advancedBus, Type messageType, string exchangeType);        
-        Task<IExchange> DeclareExchangeAsync(IAdvancedBus advancedBus, string exchangeName, string exchangeType);
+        IExchange DeclareExchange(IAdvancedBus advancedBus, Type messageType, string exchangeType);
         Task<IExchange> DeclareExchangeAsync(IAdvancedBus advancedBus, Type messageType, string exchangeType);
+    }
+
+    public interface IAdvancedPublishExchangeDeclareStrategy
+    {
+        IExchange DeclareExchange(IAdvancedBus advancedBus, string exchangeName, string exchangeType);
+        Task<IExchange> DeclareExchangeAsync(IAdvancedBus advancedBus, string exchangeName, string exchangeType);
     }
 }
