6a29082218093958140fc1212f6879fa3fbe75e2
Tordenhunden <steffen.daniel.jensen@gmail.com>
2014-07-31 16:46:31 +0200

    Fix stackoverflow in initialization.
    I turned out to have a circular dependency which crashed the service locator (stackoverflow). It seemed like such a simple commit :-)
    I had to introduce the IAdvancedRpcFactory since the implementation needs the advanced bus.
    
        interface IAdvancedRpcFactory
        {
            IAdvancedClientRpc CreateClientRpc(IAdvancedBus advancedBus);
            IAdvancedServerRpc CreateServerRpc(IAdvancedBus advancedBus);
        }
    
    But I guess it makes sense that they are defined at the same time. Then they are more obviously compatible
    
    Conflicts:
    	Source/EasyNetQ/RabbitAdvancedBus.cs

---
+++ Source/EasyNetQ/Rpc/IAdvancedRpcFactory.cs
@@ -0,0 +1,8 @@
+﻿namespace EasyNetQ.Rpc
+{
+    public interface IAdvancedRpcFactory
+    {
+        IAdvancedClientRpc CreateClientRpc(IAdvancedBus advancedBus);
+        IAdvancedServerRpc CreateServerRpc(IAdvancedBus advancedBus);
+    }
+}
