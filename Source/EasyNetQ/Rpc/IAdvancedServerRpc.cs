d4cc71ed0fa2b3688cc0b879dfc2d90ad55d07eb
Tordenhunden <steffen.daniel.jensen@gmail.com>
2014-07-31 15:42:02 +0200

    Moved rpc classes to their own files
    Conflicts:
    	Source/EasyNetQ/Rpc/IRpc.cs

---
+++ Source/EasyNetQ/Rpc/IAdvancedServerRpc.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+using EasyNetQ.Topology;
+
+namespace EasyNetQ.Rpc
+{
+    public interface IAdvancedServerRpc
+    {
+        IDisposable Respond(IExchange requestExchange, 
+                            IQueue queue, 
+                            string topic,
+                            Func<SerializedMessage, Task<SerializedMessage>> handleRequest);
+    }
+}
