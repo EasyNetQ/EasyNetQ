4408ebc4be2273403fd3dc53f56bfd2db8c797f1
Tordenhunden <steffen.daniel.jensen@gmail.com>
2014-07-31 15:42:02 +0200

    Moved rpc classes to their own files
    Conflicts:
    	Source/EasyNetQ/Rpc/IRpc.cs

---
+++ Source/EasyNetQ/Rpc/IAdvancedClientRpc.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+using EasyNetQ.Topology;
+
+namespace EasyNetQ.Rpc
+{
+    public interface IAdvancedClientRpc
+    {
+        Task<SerializedMessage> RequestAsync(IExchange requestExchange,
+                                             string requestRoutingKey,
+                                             bool mandatory,
+                                             bool immediate,
+                                             Func<string> responseQueueName,
+                                             SerializedMessage request);
+    }
+}
