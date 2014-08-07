40b7c9a63f1047a9287ddee457582a2fab2061d3
Tordenhunden <steffen.daniel.jensen@gmail.com>
2014-08-07 14:59:05 +0200

    Added stacktrace in server's exception reply
    Conflicts:
    	Source/EasyNetQ/Rpc/FreshQueue/FreshQueueServerRpc.cs

--- Source/EasyNetQ/Rpc/FreshQueue/FreshQueueServerRpc.cs
+++ Source/EasyNetQ/Rpc/FreshQueue/FreshQueueServerRpc.cs
@@ -3,15 +3,15 @@ using System.Linq;
 using System.Threading.Tasks;
 using EasyNetQ.Topology;
 
-namespace EasyNetQ.Rpc.FreshQueue
+namespace EasyNetQ.Rpc
 {
-    class FreshQueueServerRpc : IAdvancedServerRpc
+    class AdvancedServerRpc : IAdvancedServerRpc
     {
         private readonly IAdvancedBus advancedBus;
         private readonly IConnectionConfiguration configuration;
         private readonly IRpcHeaderKeys _rpcHeaderKeys;
 
-        public FreshQueueServerRpc(
+        public AdvancedServerRpc(
             IAdvancedBus advancedBus,
             IConnectionConfiguration configuration,
             IRpcHeaderKeys rpcHeaderKeys)
@@ -32,11 +32,11 @@ namespace EasyNetQ.Rpc.FreshQueue
             Preconditions.CheckNotNull(handleRequest, "handleRequest");
 
             var expires = (int)TimeSpan.FromSeconds(configuration.Timeout).TotalMilliseconds;
-            var queue = advancedBus.QueueDeclare(queueName,
-                passive: false,
-                durable: false,
+            var queue = advancedBus.QueueDeclare(queueName, 
+                passive: false, 
+                durable: false, 
                 exclusive: false,
-                autoDelete: true,
+                autoDelete: true, 
                 expires: expires);
 
             advancedBus.Bind(requestExchange, queue, topic);
@@ -44,46 +44,46 @@ namespace EasyNetQ.Rpc.FreshQueue
             var responseExchange = Exchange.GetDefault();
             return advancedBus.Consume(queue, (msgBytes, msgProp, messageRecievedInfo) => ExecuteResponder(responseExchange, handleRequest, new SerializedMessage(msgProp, msgBytes)));
         }
-
-        private Task ExecuteResponder(IExchange responseExchange, Func<SerializedMessage, Task<SerializedMessage>> responder, SerializedMessage requestMessage)
+            
+        private Task ExecuteResponder(IExchange responseExchange, Func<SerializedMessage, Task<SerializedMessage>> responder, SerializedMessage requestMessage) 
         {
             var tcs = new TaskCompletionSource<object>();
 
             responder(requestMessage).ContinueWith(SendReplyContinuation(responseExchange, requestMessage, tcs));
-
-
+            
+            
             return tcs.Task;
         }
 
         private Action<Task<SerializedMessage>> SendReplyContinuation(IExchange responseExchange, SerializedMessage requestMessage, TaskCompletionSource<object> tcs)
         {
             return task =>
-            {
-
-                if (task.IsFaulted)
                 {
-                    if (task.Exception != null)
+                    
+                    if (task.IsFaulted)
                     {
-                        var errorStackTrace = string.Join("\n\n", task.Exception.InnerExceptions.Select(e => e.StackTrace));
+                        if (task.Exception != null)
+                        {
+                            var errorStackTrace = string.Join("\n\n", task.Exception.InnerExceptions.Select(e => e.StackTrace));
 
-                        var responseMessage = new SerializedMessage(new MessageProperties(), new byte[] { });
-                        responseMessage.Properties.Headers.Add(_rpcHeaderKeys.IsFaultedKey, true);
-                        responseMessage.Properties.Headers.Add(_rpcHeaderKeys.ExceptionMessageKey, errorStackTrace);
+                            var responseMessage = new SerializedMessage(new MessageProperties(), new byte[] { });
+                            responseMessage.Properties.Headers.Add(_rpcHeaderKeys.IsFaultedKey, true);
+                            responseMessage.Properties.Headers.Add(_rpcHeaderKeys.ExceptionMessageKey, errorStackTrace);
+                            responseMessage.Properties.CorrelationId = requestMessage.Properties.CorrelationId;
+
+                            advancedBus.Publish(responseExchange, requestMessage.Properties.ReplyTo, false, false, responseMessage.Properties, requestMessage.Body);
+                            tcs.SetException(task.Exception);
+                        }
+                    }
+                    else
+                    {
+                        var responseMessage = task.Result;
                         responseMessage.Properties.CorrelationId = requestMessage.Properties.CorrelationId;
 
-                        advancedBus.Publish(responseExchange, requestMessage.Properties.ReplyTo, false, false, responseMessage.Properties, requestMessage.Body);
-                        tcs.SetException(task.Exception);
+                        advancedBus.Publish(responseExchange, requestMessage.Properties.ReplyTo, false, false, responseMessage.Properties, responseMessage.Body);
+                        tcs.SetResult(null);
                     }
-                }
-                else
-                {
-                    var responseMessage = task.Result;
-                    responseMessage.Properties.CorrelationId = requestMessage.Properties.CorrelationId;
-
-                    advancedBus.Publish(responseExchange, requestMessage.Properties.ReplyTo, false, false, responseMessage.Properties, responseMessage.Body);
-                    tcs.SetResult(null);
-                }
-            };
+                };
         }
     }
 }
