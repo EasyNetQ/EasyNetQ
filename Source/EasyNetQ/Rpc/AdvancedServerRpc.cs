46bd0d9a4c4ecdee4a0d6553b6c92de7f866e8d3
Tordenhunden <steffen.daniel.jensen@gmail.com>
2014-08-08 10:32:21 +0200

    Rpc server declares exchange

--- Source/EasyNetQ/Rpc/AdvancedServerRpc.cs
+++ Source/EasyNetQ/Rpc/AdvancedServerRpc.cs
@@ -6,8 +6,8 @@ namespace EasyNetQ.Rpc
 {
     class AdvancedServerRpc : IAdvancedServerRpc
     {
-        private readonly IAdvancedBus advancedBus;
-        private readonly IConnectionConfiguration configuration;
+        private readonly IAdvancedBus _advancedBus;
+        private readonly IConnectionConfiguration _configuration;
         private readonly IRpcHeaderKeys _rpcHeaderKeys;
 
         public AdvancedServerRpc(
@@ -18,8 +18,8 @@ namespace EasyNetQ.Rpc
             Preconditions.CheckNotNull(advancedBus, "advancedBus");
             Preconditions.CheckNotNull(configuration, "configuration");
 
-            this.advancedBus = advancedBus;
-            this.configuration = configuration;
+            this._advancedBus = advancedBus;
+            this._configuration = configuration;
             _rpcHeaderKeys = rpcHeaderKeys;
         }
 
@@ -30,20 +30,21 @@ namespace EasyNetQ.Rpc
             Preconditions.CheckNotNull(topic, "topic");
             Preconditions.CheckNotNull(handleRequest, "handleRequest");
 
-            var expires = (int)TimeSpan.FromSeconds(configuration.Timeout).TotalMilliseconds;
-            
-            //TODO declare exchange
-            var queue = advancedBus.QueueDeclare(queueName, 
+            var expires = (int)TimeSpan.FromSeconds(_configuration.Timeout).TotalMilliseconds;
+
+            _advancedBus.ExchangeDeclare(requestExchange.Name, ExchangeType.Topic);
+
+            var queue = _advancedBus.QueueDeclare(queueName, 
                 passive: false, 
                 durable: false, 
                 exclusive: false,
                 autoDelete: true, 
                 expires: expires);
 
-            advancedBus.Bind(requestExchange, queue, topic);
+            _advancedBus.Bind(requestExchange, queue, topic);
 
             var responseExchange = Exchange.GetDefault();
-            return advancedBus.Consume(queue, (msgBytes, msgProp, messageRecievedInfo) => ExecuteResponder(responseExchange, handleRequest, new SerializedMessage(msgProp, msgBytes)));
+            return _advancedBus.Consume(queue, (msgBytes, msgProp, messageRecievedInfo) => ExecuteResponder(responseExchange, handleRequest, new SerializedMessage(msgProp, msgBytes)));
         }
             
         private Task ExecuteResponder(IExchange responseExchange, Func<SerializedMessage, Task<SerializedMessage>> responder, SerializedMessage requestMessage)
@@ -54,7 +55,7 @@ namespace EasyNetQ.Rpc
                     {
                         var sm = uhInfo.Response;
                         sm.Properties.CorrelationId = requestMessage.Properties.CorrelationId;
-                        advancedBus.Publish(responseExchange, requestMessage.Properties.ReplyTo, false, false, sm.Properties, sm.Body);
+                        _advancedBus.Publish(responseExchange, requestMessage.Properties.ReplyTo, false, false, sm.Properties, sm.Body);
                         return TaskHelpers.FromResult(uhInfo);
                     })
                 .Then(uhInfo => 
