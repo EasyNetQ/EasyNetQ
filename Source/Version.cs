95dff80b777fb098098a457ecf8d82d0b13c0e7c
Nicholas Weise Oxhøj <noxhj@ebay.com>
2014-09-27 12:16:04 +0200

    Bumped version number from 0.36.1.0 to 0.36.2.0

--- Source/Version.cs
+++ Source/Version.cs
@@ -2,11 +2,12 @@
 using System.Reflection;
 
 // EasyNetQ version number: <major>.<minor>.<non-breaking-feature>.<build>
-[assembly: AssemblyVersion("0.36.1.0")]
+[assembly: AssemblyVersion("0.36.2.0")]
 [assembly: CLSCompliant(true)]
 
 // Note: until version 1.0 expect breaking changes on 0.X versions.
 
+// 0.36.2.0 Fixed threading issue in EventBus
 // 0.36.1.0 Updated Json.Net to the latest version
 // 0.36.0.0 Support for blocked connection notifications
 // 0.35.5.0 Basic implementation of produce-consumer interception
