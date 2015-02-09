b0d9e1758a07466eb0e8fb1a1e3ca71be0f8d51b
Rob Hruska <robhruska@gmail.com>
2015-02-09 15:03:32 -0600

    Doc comments on ISendReceive for configure property

--- Source/EasyNetQ/Producer/ISendReceive.cs
+++ Source/EasyNetQ/Producer/ISendReceive.cs
@@ -31,7 +31,14 @@ namespace EasyNetQ.Producer
         /// <returns>Consumer cancellation. Call Dispose to stop consuming</returns>
         IDisposable Receive<T>(string queue, Action<T> onMessage) where T : class;
 
-        // TODO doc
+        /// <summary>
+        /// Receive a message from the specified queue
+        /// </summary>
+        /// <typeparam name="T">The type of message to receive</typeparam>
+        /// <param name="queue">The queue to receive from</param>
+        /// <param name="onMessage">The synchronous function that handles the message</param>
+        /// <param name="configure">Action to configure consumer with</param>
+        /// <returns>Consumer cancellation. Call Dispose to stop consuming</returns>
         IDisposable Receive<T>(string queue, Action<T> onMessage, Action<IConsumerConfiguration> configure) where T : class;
 
         /// <summary>
@@ -43,7 +50,14 @@ namespace EasyNetQ.Producer
         /// <returns>Consumer cancellation. Call Dispose to stop consuming</returns>
         IDisposable Receive<T>(string queue, Func<T, Task> onMessage) where T : class;
 
-        // TODO doc
+        /// <summary>
+        /// Receive a message from the specified queue
+        /// </summary>
+        /// <typeparam name="T">The type of message to receive</typeparam>
+        /// <param name="queue">The queue to receive from</param>
+        /// <param name="onMessage">The asynchronous function that handles the message</param>
+        /// <param name="configure">Action to configure consumer with</param>
+        /// <returns>Consumer cancellation. Call Dispose to stop consuming</returns>
         IDisposable Receive<T>(string queue, Func<T, Task> onMessage, Action<IConsumerConfiguration> configure) where T : class;
 
         /// <summary>
@@ -54,7 +68,13 @@ namespace EasyNetQ.Producer
         /// <returns>Consumer cancellation. Call Dispose to stop consuming</returns>
         IDisposable Receive(string queue, Action<IReceiveRegistration> addHandlers);
 
-        // TODO doc
+        /// <summary>
+        /// Receive a message from the specified queue. Dispatch them to the given handlers
+        /// </summary>
+        /// <param name="queue">The queue to take messages from</param>
+        /// <param name="addHandlers">A function to add handlers</param>
+        /// <param name="configure">Action to configure consumer with</param>
+        /// <returns>Consumer cancellation. Call Dispose to stop consuming</returns>
         IDisposable Receive(string queue, Action<IReceiveRegistration> addHandlers, Action<IConsumerConfiguration> configure);
     }
 }
