aa7617344a2bf7767d149b86602fd199ebffe213
Mathieu Leenhardt <mathieu.leenhardt@gmail.com>
2015-03-03 23:56:23 +0100

    Bumped version

--- Source/Version.cs
+++ Source/Version.cs
@@ -2,11 +2,12 @@
 using System.Reflection;
 
 // EasyNetQ version number: <major>.<minor>.<non-breaking-feature>.<build>
-[assembly: AssemblyVersion("0.46.1.0")]
+[assembly: AssemblyVersion("0.47.0.0")]
 [assembly: CLSCompliant(true)]
 
 // Note: until version 1.0 expect breaking changes on 0.X versions.
 
+// 0.47.0.0 It's now required to call PersistentConnection.InitAsync() to bootstrap a PersistentConnection and make it start attempting to connect.
 // 0.46.1.0 Fix NullReferenceException on Serialize
 // 0.46.0.0 Implementation of AdvancedBusEventHandlers and events are gone from IBus.
 // 0.45.0.0 IBus Subscription methods now return an ISubscriptionResult and IAdvancedBus exposes IConventions.
