abfbb318fc051559a7b00ec77e4ff3eb9be93363
Mathieu Leenhardt <mathieu.leenhardt@gmail.com>
2015-03-21 17:52:03 +0100

    typenameserializer uses ConcurrentDictionary

--- Source/EasyNetQ/TypeNameSerializer.cs
+++ Source/EasyNetQ/TypeNameSerializer.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Concurrent;
 
 namespace EasyNetQ
 {
@@ -10,37 +11,44 @@ namespace EasyNetQ
 
     public class TypeNameSerializer : ITypeNameSerializer
     {
+        private readonly ConcurrentDictionary<string, Type> deserializedTypes = new ConcurrentDictionary<string, Type>();
+
         public Type DeSerialize(string typeName)
         {
             Preconditions.CheckNotBlank(typeName, "typeName");
-            var nameParts = typeName.Split(':');
-            if (nameParts.Length != 2)
+
+            return deserializedTypes.GetOrAdd(typeName, t =>
             {
-                throw new EasyNetQException(
-                    "type name {0}, is not a valid EasyNetQ type name. Expected Type:Assembly", 
-                    typeName);
-            }
-            var type = Type.GetType(nameParts[0] + ", " + nameParts[1]);
-            if (type == null)
-            {
-                throw new EasyNetQException(
-                    "Cannot find type {0}",
-                    typeName);
-            }
-            return type;
+                var nameParts = t.Split(':');
+                if (nameParts.Length != 2)
+                {
+                    throw new EasyNetQException("type name {0}, is not a valid EasyNetQ type name. Expected Type:Assembly", t);
+                }
+                var type = Type.GetType(nameParts[0] + ", " + nameParts[1]);
+                if (type == null)
+                {
+                    throw new EasyNetQException("Cannot find type {0}", t);
+                }
+                return type;
+            });
         }
+
+        private readonly ConcurrentDictionary<Type, string> serializedTypes = new ConcurrentDictionary<Type, string>();
 
         public string Serialize(Type type)
         {
             Preconditions.CheckNotNull(type, "type");
-            var typeName = type.FullName + ":" + type.Assembly.GetName().Name;
-            if (typeName.Length > 255)
+
+            return serializedTypes.GetOrAdd(type, t =>
             {
-                throw new EasyNetQException("The serialized name of type '{0}' exceeds the AMQP " + 
-                    "maximum short string length of 255 characters.",
-                    type.Name);
-            }
-            return typeName;
+                var typeName = t.FullName + ":" + t.Assembly.GetName().Name;
+                if (typeName.Length > 255)
+                {
+                    throw new EasyNetQException("The serialized name of type '{0}' exceeds the AMQP " +
+                                                "maximum short string length of 255 characters.", t.Name);
+                }
+                return typeName;
+            });
         }
     }
 }
