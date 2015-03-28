7b18c4b726414942009640c2c7e5ed0a470bf06a
Mathieu Leenhardt <mathieu.leenhardt@gmail.com>
2015-03-28 15:08:15 +0100

    Updated version

--- Source/Version.cs
+++ Source/Version.cs
@@ -2,11 +2,12 @@
 using System.Reflection;
 
 // EasyNetQ version number: <major>.<minor>.<non-breaking-feature>.<build>
-[assembly: AssemblyVersion("0.47.8.0")]
+[assembly: AssemblyVersion("0.48.0.0")]
 [assembly: CLSCompliant(true)]
 
 // Note: until version 1.0 expect breaking changes on 0.X versions.
 
+// 0.48.0.0 Updated to RabbitMQ.Client 3.5
 // 0.47.8.0 Rpc.Respond will validate serialized length of TResponse upon method call to prevent silent exception when executing responder.
 // 0.47.7.0 Validating ConnectionConfiguration in lowest level method of RabbitHutch.
 // 0.47.6.0 AtLeastOneWithDefault -> DefaultIfEmpty
