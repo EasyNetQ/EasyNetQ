e0106a76149a0530795c97d2dda9c6edd3e7e2da
mwojcik@travelrepublic.co.uk <mwojcik@travelrepublic.co.uk>
2015-04-20 12:22:42 +0100

    Integration tests for DelayedExchange implementation.

--- Source/EasyNetQ/DelayedExchangeScheduler.cs
+++ Source/EasyNetQ/DelayedExchangeScheduler.cs
@@ -9,7 +9,8 @@ namespace EasyNetQ
 {
     public class DelayedExchangeScheduler : IScheduler
     {
-                private readonly IAdvancedBus advancedBus;
+        private const string CancellationNotSupportedErrorMessage = "Future messages cancellation is not suuported by Delayed Exchange Scheduler. If you need this functionality, please configure bus to use Scheduler instead.";
+        private readonly IAdvancedBus advancedBus;
         private readonly IConventions conventions;
         private readonly IMessageDeliveryModeStrategy messageDeliveryModeStrategy;
         private readonly IPublishExchangeDeclareStrategy publishExchangeDeclareStrategy;
@@ -43,12 +44,16 @@ namespace EasyNetQ
 
         public void FuturePublish<T>(DateTime futurePublishDate, T message) where T : class
         {
-            FuturePublish(futurePublishDate, null, message);
+            var messageDelay = futurePublishDate.Subtract(DateTime.UtcNow);
+            FuturePublishAsync(messageDelay, message).Wait();
         }
 
         public void FuturePublish<T>(DateTime futurePublishDate, string cancellationKey, T message) where T : class
         {
-            FuturePublishAsync(futurePublishDate, cancellationKey, message).Wait();
+            if (cancellationKey != null)
+                throw new NotImplementedException(CancellationNotSupportedErrorMessage);
+
+            FuturePublish(futurePublishDate, message);
         }
 
         public void FuturePublish<T>(TimeSpan messageDelay, T message) where T : class
@@ -58,7 +63,7 @@ namespace EasyNetQ
 
         public void CancelFuturePublish(string cancellationKey)
         {
-            throw new NotImplementedException();
+            throw new NotImplementedException(CancellationNotSupportedErrorMessage);
         }
 
         public Task FuturePublishAsync<T>(DateTime futurePublishDate, T message) where T : class
@@ -68,39 +73,46 @@ namespace EasyNetQ
 
         public Task FuturePublishAsync<T>(DateTime futurePublishDate, string cancellationKey, T message) where T : class
         {
-            var delay = futurePublishDate.Subtract(DateTime.UtcNow);
-            return FuturePublishAsync(delay, message);
+            if (cancellationKey != null)
+                throw new NotImplementedException(CancellationNotSupportedErrorMessage);
+
+            return FuturePublishAsync(futurePublishDate, message);
         }
 
         public Task FuturePublishAsync<T>(TimeSpan messageDelay, T message) where T : class
         {
             Preconditions.CheckNotNull(message, "message");
 
-            //var delay = Round(messageDelay);
-            //var delayString = delay.ToString(@"dd\_hh\_mm\_ss");
-            var exchangeName = conventions.ExchangeNamingConvention(typeof(T)) + "_delayed";
-            //var futureExchangeName = exchangeName + "_" + delayString;
-            var queueName = conventions.QueueNamingConvention(typeof(T), String.Empty);
-            return advancedBus.ExchangeDeclareAsync(exchangeName, ExchangeType.Topic, delayed: true)
-                .Then(exchange => advancedBus.QueueDeclareAsync(queueName)
-                                                   .Then(queue => advancedBus.BindAsync(exchange, queue, "#"))
-                                                   .Then(() =>
-                                                   {
-                                                       var easyNetQMessage = new Message<T>(message)
-                                                       {
-                                                           Properties =
-                                                           {
-                                                               DeliveryMode = messageDeliveryModeStrategy.GetDeliveryMode(typeof(T)),
-                                                               Headers = new Dictionary<string, object> { { "x-delay", (int)messageDelay.TotalMilliseconds } }
-                                                           }
-                                                       };
-                                                       return advancedBus.PublishAsync(exchange, "#", false, false, easyNetQMessage);
-                                                   }));
+            var exchangeName = conventions.ExchangeNamingConvention(typeof (T));
+            var futureExchangeName = exchangeName + "_delayed";
+            var queueName = conventions.QueueNamingConvention(typeof (T), String.Empty);
+
+            return advancedBus.ExchangeDeclareAsync(futureExchangeName, ExchangeType.Direct, delayed: true)
+                .Then(futureExchange => advancedBus.ExchangeDeclareAsync(exchangeName, ExchangeType.Topic)
+                    .Then(exchange => advancedBus.BindAsync(futureExchange, exchange, "#")
+                        .Then(binding => advancedBus.QueueDeclareAsync(queueName)
+                            .Then(queue => advancedBus.BindAsync(exchange, queue, "#"))
+                            .Then(() =>
+                            {
+                                var easyNetQMessage = new Message<T>(message)
+                                {
+                                    Properties =
+                                    {
+                                        DeliveryMode = messageDeliveryModeStrategy.GetDeliveryMode(typeof (T)),
+                                        Headers =
+                                            new Dictionary<string, object> {{"x-delay", (int) messageDelay.TotalMilliseconds}}
+                                    }
+                                };
+                                return advancedBus.PublishAsync(futureExchange, "#", false, false, easyNetQMessage);
+                            })
+                        )
+                    )
+                );
         }
 
         public Task CancelFuturePublishAsync(string cancellationKey)
         {
-            throw new NotImplementedException();
+            throw new NotImplementedException(CancellationNotSupportedErrorMessage);
         }
     }
 }
