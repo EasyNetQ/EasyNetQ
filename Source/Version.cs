c0b21ea5f269299eb0a7a39bbcd1533c4a60c040
alexwiese <aleX@wiese.com.au>
2015-05-02 18:00:23 +0930

    Added Start Consuming Events Added StartConsumingFailedEvent and StartConsumingSucceededEvent. These Events are fired by the consumer when it attempts to start consuming.

--- Source/Version.cs
+++ Source/Version.cs
@@ -2,15 +2,15 @@
 using System.Reflection;
 
 // EasyNetQ follows the Semantic Versioning 2.0.0 (http://semver.org/)
-
 // Given a version number MAJOR.MINOR.PATCH, increment the:
 // MAJOR version when you make incompatible API changes,
 // MINOR version when you add functionality in a backwards-compatible manner, and
 // PATCH version when you make backwards-compatible bug fixes.
 
-[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyVersion("1.0.2.0")]
 [assembly: CLSCompliant(false)]
 
+// 1.0.2.0 Added start consuming events for failure and success
 // 1.0.1.0  First stable release
 // 0.63.6.0 Added support for multiple exchange (creating an exchange per implemented interface for a concreate type)
 // 0.63.5.0 Made some methods protected/virtual in HandlerRunner so we can override the behavior
