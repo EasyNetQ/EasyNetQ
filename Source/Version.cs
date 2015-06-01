89b78f2c25ca65fa021cde0e9d8e86cd2fd5f84f
Yury Pliner <yury.pliner@gmail.com>
2015-06-01 19:52:18 +0500

    Bump version

--- Source/Version.cs
+++ Source/Version.cs
@@ -2,12 +2,12 @@
 using System.Reflection;
 
 // EasyNetQ version number: <major>.<minor>.<non-breaking-feature>.<build>
-[assembly: AssemblyVersion("0.49.4.0")]
+[assembly: AssemblyVersion("0.50.0.0")]
 [assembly: CLSCompliant(true)]
 
 // Note: until version 1.0 expect breaking changes on 0.X versions.
 
-// 0.49.4.0 Updated to RabbitMQ.Client 3.5.3
+// 0.50.0.0 Updated to RabbitMQ.Client 3.5.3
 // 0.49.3.0 Polymorphic publish now works with Scheduler.Mongo
 // 0.49.2.0 Fix subscription for events if queues were created in previous versions
 // 0.49.1.0 Priority queue support
