08ede2084e950c003b3cb36c391d6ebce17b70ff
Scott Koon <skoon@russell.com>
2015-06-15 16:22:15 -0700

    Added in an extension overload for IAdvancedBus for Autofac

--- Source/EasyNetQ.DI.Autofac/InjectionExtensions.cs
+++ Source/EasyNetQ.DI.Autofac/InjectionExtensions.cs
@@ -8,7 +8,22 @@ namespace EasyNetQ.DI
         public static Autofac.IContainer RegisterAsEasyNetQContainerFactory(this ContainerBuilder builder, Func<IBus> busCreator)
         {
             var adapter = new AutofacAdapter(builder);
-            
+
+            RabbitHutch.SetContainerFactory(() => adapter);
+
+            var container = adapter.Container;
+
+            var bus = busCreator();
+
+            adapter.Register(provider => bus);
+
+            return container;
+        }
+
+        public static Autofac.IContainer RegisterAsEasyNetQContainerFactory(this ContainerBuilder builder, Func<IAdvancedBus> busCreator)
+        {
+            var adapter = new EasyNetQ.DI.AutofacAdapter(builder);
+
             RabbitHutch.SetContainerFactory(() => adapter);
 
             var container = adapter.Container;
