acb87682ec38a9aefb56a9d19d7234096edff462
Michael Denny <micdenny@gmail.com>
2015-11-18 16:14:27 +0100

    bumped version to 0.52.0

--- Source/Version.cs
+++ Source/Version.cs
@@ -2,11 +2,12 @@
 using System.Reflection;
 
 // EasyNetQ version number: <major>.<minor>.<non-breaking-feature>.<build>
-[assembly: AssemblyVersion("0.51.0.0")]
+[assembly: AssemblyVersion("0.52.0.0")]
 [assembly: CLSCompliant(true)]
 
 // Note: until version 1.0 expect breaking changes on 0.X versions.
 
+// 0.52.0.0 Added synchronous callback on Consume(byte[]) methods of the advaced api
 // 0.51.0.0 Brand new sync/async implementation, a lot of changes in publish mechanisms. Should be used with care  
 // 0.50.12.0 Added Serilog nuget package 
 // 0.50.11.0 Updated Scheduler for MSSQL to support all properties of the ScheduleMe message 
