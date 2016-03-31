3505935c0b5662180b6ce5c90c44b1be3aadc3c2
Joel Weiss <2joelweiss@gmail.com>
2016-03-31 12:56:40 -0400

    Reset <non-breaking-feature> version number to 0

--- Source/Version.cs
+++ Source/Version.cs
@@ -2,7 +2,7 @@
 using System.Reflection;
 
 // EasyNetQ version number: <major>.<minor>.<non-breaking-feature>.<build>
-[assembly: AssemblyVersion("0.56.1.0")]
+[assembly: AssemblyVersion("0.56.0.0")]
 [assembly: CLSCompliant(false)]
 
 // Note: until version 1.0 expect breaking changes on 0.X versions.
