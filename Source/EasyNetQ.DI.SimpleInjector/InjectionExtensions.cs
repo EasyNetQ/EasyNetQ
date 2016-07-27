37152cc2f754acc239d83f5ce7188e9e1088dc97
Georg Pfeiffer <g.pfeiffer@emakina.at>
2016-07-27 17:43:07 +0200

    add SimpleInjector support to the project

---
+++ Source/EasyNetQ.DI.SimpleInjector/InjectionExtensions.cs
@@ -0,0 +1,12 @@
+﻿using SimpleInjector;
+
+namespace EasyNetQ.DI
+{
+    public static class InjectionExtensions
+    {
+        public static void RegisterAsEasyNetQContainerFactory(this Container container)
+        {
+            RabbitHutch.SetContainerFactory(() => new SimpleInjectorAdapter(container));
+        }
+    }
+}
