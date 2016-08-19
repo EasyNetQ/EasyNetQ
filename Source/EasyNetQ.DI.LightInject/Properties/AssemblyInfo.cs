83fb229111461cbda47ea3bd457f4136ce4703a9
Michael Denny <micdenny@gmail.com>
2016-08-20 00:23:46 +0200

    EasyNetQ.DI.LightInject => netcore

--- Source/EasyNetQ.DI.LightInject/Properties/AssemblyInfo.cs
+++ Source/EasyNetQ.DI.LightInject/Properties/AssemblyInfo.cs
@@ -1,26 +1,19 @@
-using System.Reflection;
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("EasyNetQ.DI.LightInject")]
-[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("EasyNetQ")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("EasyNetQ.DI.LightInject")]
-[assembly: AssemblyCopyright("Copyright © EasyNetQ 2016")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("3ad2cd4a-8969-4c2d-8eaa-9fc800585678")]
-
-[assembly: AssemblyVersion("2.0.2.0")]
-[assembly: AssemblyInformationalVersion("2.0.2-radicalgeek-netcore.1+1396.Branch.radicalgeek-netcore.Sha.e65133898ce725596ee8c983c941aeac1bb07e8c")]
-[assembly: AssemblyFileVersion("2.0.2.0")]
+[assembly: Guid("caa56dec-6d0c-4141-b7dc-66db1bb1ef72")]
