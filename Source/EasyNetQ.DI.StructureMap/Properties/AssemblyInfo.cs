39d8742b4ee217e03fe533d086e80ee59fd9f2da
Michael Denny <micdenny@gmail.com>
2016-08-20 14:09:03 +0200

    EasyNetQ.DI.StructureMap => netcore

--- Source/EasyNetQ.DI.StructureMap/Properties/AssemblyInfo.cs
+++ Source/EasyNetQ.DI.StructureMap/Properties/AssemblyInfo.cs
@@ -1,25 +1,19 @@
-using System.Reflection;
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("EasyNetQ.DI.StructureMap")]
-[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("EasyNetQ")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("EasyNetQ.DI.StructureMap")]
-[assembly: AssemblyCopyright("Copyright © EasyNetQ 2013")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("7233ff8e-5b46-4133-a2c3-de69cf51b0a5")]
-[assembly: AssemblyVersion("2.0.2.0")]
-[assembly: AssemblyInformationalVersion("2.0.2-radicalgeek-netcore.1+1396.Branch.radicalgeek-netcore.Sha.e65133898ce725596ee8c983c941aeac1bb07e8c")]
-[assembly: AssemblyFileVersion("2.0.2.0")]
+[assembly: Guid("0ebe99ee-56d8-47c0-9ed6-c6bcc88c4453")]
