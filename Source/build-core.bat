9f5319bb21186c3b21db332de08199e27c49b4c7
Michael Denny <micdenny@gmail.com>
2016-08-21 21:33:08 +0200

    update build-core.bat

--- Source/build-core.bat
+++ Source/build-core.bat
@@ -1,3 +1,18 @@
-rm **\bin\* -re -fo
 gitversion /output buildserver /updateassemblyinfo
-call EasyNetQ.Core\build.cmd 
+set basePath=%~dp0
+cd %basePath%EasyNetQ
+call build.cmd
+cd %basePath%EasyNetQ.DI.Autofac
+call build.cmd
+cd %basePath%EasyNetQ.DI.LightInject
+call build.cmd
+cd %basePath%EasyNetQ.DI.Ninject
+call build.cmd
+cd %basePath%EasyNetQ.DI.SimpleInjector
+call build.cmd
+cd %basePath%EasyNetQ.DI.StructureMap
+call build.cmd
+cd %basePath%EasyNetQ.DI.Windsor
+call build.cmd
+cd %basePath%EasyNetQ.Serilog
+call build.cmd
