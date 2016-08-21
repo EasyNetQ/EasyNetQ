e2cb9918e8cff738315716ccef2e24a6144ee7f7
Michael Denny <micdenny@gmail.com>
2016-08-21 03:56:48 +0200

    EasyNetQ.Serilog => netcore

--- Source/EasyNetQ.Serilog/Properties/AssemblyInfo.cs
+++ Source/EasyNetQ.Serilog/Properties/AssemblyInfo.cs
@@ -1,37 +1,19 @@
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("EasyNetQ.Serilog")]
-[assembly: AssemblyDescription("EasyNetQ Serilog implementation")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("EasyNetQ.Serilog")]
-[assembly: AssemblyCopyright("Copyright © Wiebe Tijsma, 2015")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("01b7e76d-df00-4f84-8df4-1b8508fefea8")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-[assembly: AssemblyFileVersion("2.0.2.0")]
-
-[assembly: AssemblyVersion("2.0.2.0")]
-[assembly: AssemblyInformationalVersion("2.0.2-radicalgeek-netcore.1+1396.Branch.radicalgeek-netcore.Sha.e65133898ce725596ee8c983c941aeac1bb07e8c")]
+[assembly: Guid("23a533be-64fb-47b2-b9d6-af48644162dd")]
