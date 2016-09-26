ed7ad84816e95372da7ebef86d8af2c257af8fd9
Jimmy Weibel Rasmussen <jwe@danskecommodities.com>
2016-09-26 16:30:04 +0200

    added the PersistentConnectionFactory

---
+++ Source/EasyNetQ/PersistentConnectionFactory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasyNetQ
+{
+    public class PersistentConnectionFactory : IPersistentConnectionFactory
+    {
+        private readonly IEventBus eventBus;
+        private readonly IConnectionFactory connectionFactory;
+        private readonly IEasyNetQLogger logger;
+
+        public PersistentConnectionFactory(IEasyNetQLogger logger, IConnectionFactory connectionFactory, IEventBus eventBus)
+        {
+            this.logger = logger;
+            this.connectionFactory = connectionFactory;
+            this.eventBus = eventBus;
+        }
+
+        public IPersistentConnection CreateConnection()
+        {
+            return new PersistentConnection(connectionFactory, logger, eventBus);
+        }
+    }
+}
