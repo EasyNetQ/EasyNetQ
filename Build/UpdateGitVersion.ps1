f64ecf2f621f5217f1aa9cae129fcbe91d2a7c18
Alina Popa <alpopa@microsoft.com>
2016-11-29 14:13:43 -0800

    Create packages out of feature/netcore branch

---
+++ Build/UpdateGitVersion.ps1
@@ -0,0 +1,26 @@
+﻿$repositoryRoot = [IO.Path]::GetFullPath($(Join-Path $PSScriptRoot "\..\"))
+$sourceFolder = Join-Path $repositoryRoot "Source"
+
+Write-Host "Repository: $repositoryRoot"
+Write-Host "Source: $sourceFolder"
+
+if (!(Test-Path $sourceFolder)) {
+    Write-Error "Cannot find $sourceFolder"
+}
+
+$projects = Get-ChildItem $sourceFolder | ? { $_.PSIsContainer -and !$_.Name.Contains("Tests") -and !$_.Name.Contains("Hosepipe.Setup") }
+
+foreach ($projectJson in $projects) {
+
+    $directory = $projectJson.FullName
+        
+    pushd $directory
+
+    if (!(Test-Path $(Join-Path $directory "project.lock.json"))) {
+        & dotnet restore
+    }
+    
+    & dotnet gitversion
+
+    popd
+}
