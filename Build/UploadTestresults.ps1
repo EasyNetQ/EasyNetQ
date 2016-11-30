639c8acea8123a5bf235285697f9bd8450a876db
Alina Popa <alpopa@microsoft.com>
2016-11-30 02:34:10 -0800

    Create packages out of feature/netcore branch (#643)
    * Create packages out of feature/netcore branch
    
    * Set server path

---
+++ Build/UploadTestresults.ps1
@@ -0,0 +1,23 @@
+﻿$repositoryRoot = [IO.Path]::GetFullPath($(Join-Path $PSScriptRoot "\..\"))
+$sourceFolder = Join-Path $repositoryRoot "Source"
+
+Write-Host "Repository: $repositoryRoot"
+Write-Host "Source: $sourceFolder"
+
+if (!(Test-Path $sourceFolder)) {
+    Write-Error "Cannot find $sourceFolder"
+}
+
+$projects = Get-ChildItem $sourceFolder | ? { $_.PSIsContainer -and $_.Name.Contains("Tests") -and !$_.Name.Contains("Tests.") }
+
+# upload results to AppVeyor
+$wc = New-Object 'System.Net.WebClient'
+
+foreach ($projectJson in $projects) {
+
+    $directory = $projectJson.FullName
+
+    $wc.UploadFile("https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)", (Join-Path $directory "\TestResult.xml"))
+
+}
+
