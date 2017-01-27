720982a4d268292e2932c337ccd5bfee6babb3ba
Endan Wang <endan83@gmail.com>
2017-01-27 21:10:09 +0000

    create an exchange for each implemented interface of specified message type, and bind it to the source exchange which is based on the message type

---
+++ Source/EasyNetQ/AdvancedMessagePolymorphism/AdvancedMessagePolymorphismExtension.cs
@@ -0,0 +1,13 @@
+﻿using EasyNetQ.Producer;
+
+namespace EasyNetQ.AdvancedMessagePolymorphism
+{
+    public static class AdvancedMessagePolymorphismExtension
+    {
+        public static IServiceRegister EnableAdvancedMessagePolymorphism(this IServiceRegister serviceRegister)
+        {
+            return serviceRegister
+                .Register<IPublishExchangeDeclareStrategy, AdvancedMessagePolymorphismPublishExchangeDeclareStrategy>();
+        }
+    }
+}
