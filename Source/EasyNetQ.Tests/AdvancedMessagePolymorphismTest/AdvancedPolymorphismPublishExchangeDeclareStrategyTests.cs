70090d864911e9c62aec889a84a7c7140d1ac797
Endan Wang <endan83@gmail.com>
2017-01-30 15:40:16 +0000

    Added first unit test

--- Source/EasyNetQ.Tests/AdvancedMessagePolymorphismTest/AdvancedPolymorphismPublishExchangeDeclareStrategyTests.cs
+++ Source/EasyNetQ.Tests/AdvancedMessagePolymorphismTest/AdvancedPolymorphismPublishExchangeDeclareStrategyTests.cs
@@ -17,39 +17,62 @@ namespace EasyNetQ.Tests.AdvancedMessagePolymorphismTest
         [Test]
         public void When_declaring_exchanges_for_message_type_that_has_no_interface_one_exchange_created()
         {
-            var bus = MockRepository.GenerateStub<IAdvancedBus>();
+            var advancedBus = MockRepository.GenerateStub<IAdvancedBus>();
 
-            bus.Stub(b => b.ExchangeDeclareAsync(Arg<IExchange>.Is.Anything, Arg<IExchange>.Is.Anything, Arg<string>.Is.Equal("#")))
+            advancedBus.Stub(b => b.ExchangeDeclareAsync(
+                Arg<string>.Is.Anything, 
+                Arg<string>.Is.Anything,
+                Arg<bool>.Is.Anything,
+                Arg<bool>.Is.Anything,
+                Arg<bool>.Is.Anything,
+                Arg<bool>.Is.Anything,
+                Arg<string>.Is.Anything,
+                Arg<bool>.Is.Anything))
+                .Return(Task.FromResult<IExchange>(MockRepository.GenerateStub<IExchange>()));
+
+            advancedBus.Stub(b => b.BindAsync(Arg<IExchange>.Is.Anything, Arg<IExchange>.Is.Anything, Arg<string>.Is.Anything))
                 .Return(Task.FromResult<IBinding>(MockRepository.GenerateStub<IBinding>()));
 
+            advancedBus.Stub(c => c.Container.Resolve<IConventions>())
+                .Return(new Conventions(new TypeNameSerializer()));
+
             var publishExchangeStrategy = new AdvancedPolymorphismPublishExchangeDeclareStrategy();
 
-            publishExchangeStrategy.DeclareExchangeAsync(bus, typeof(MyMessage), ExchangeType.Topic).Wait();
 
-            bus.AssertWasCalled(b => b.BindAsync())
+            publishExchangeStrategy.DeclareExchangeAsync(advancedBus, typeof(MyMessage), ExchangeType.Topic).Wait();
 
-            Assert.That(exchanges, Has.Count.EqualTo(1), "Single exchange should have been created");
-            Assert.That(exchanges[0].Name, Is.EqualTo("MyMessage"), "Exchange should have used naming convection to name the exchange");
-            Assert.That(exchanges[0].BoundTo, Is.Null, "Unversioned message should not create any exchange to exchange bindings");
+            //ensure that only one exchange is declared
+            advancedBus.AssertWasCalled(m => m.ExchangeDeclareAsync(
+                Arg<string>.Is.Anything,
+                Arg<string>.Is.Anything,
+                Arg<bool>.Is.Anything,
+                Arg<bool>.Is.Anything,
+                Arg<bool>.Is.Anything,
+                Arg<bool>.Is.Anything,
+                Arg<string>.Is.Anything,
+                Arg<bool>.Is.Anything), opt => opt.Repeat.Times(1));
+
+            //ensure that the exchange is not bound to anything
+            advancedBus.AssertWasNotCalled(b => b.BindAsync(Arg<IExchange>.Is.Anything, Arg<IExchange>.Is.Anything, Arg<string>.Is.Anything));
         }
 
-        [Test]
-        public void When_declaring_exchanges_for_versioned_message_exchange_per_version_created_and_bound_to_superceding_version()
-        {
-            var exchanges = new List<ExchangeStub>();
-            var bus = CreateAdvancedBusMock(exchanges.Add, BindExchanges, t => t.Name);
-            var publishExchangeStrategy = new AdvancedPolymorphismPublishExchangeDeclareStrategy();
+        //[Test]
+        //public void When_declaring_exchanges_for_versioned_message_exchange_per_version_created_and_bound_to_superceding_version()
+        //{
+        //    var exchanges = new List<ExchangeStub>();
+        //    var bus = CreateAdvancedBusMock(exchanges.Add, BindExchanges, t => t.Name);
+        //    var publishExchangeStrategy = new AdvancedPolymorphismPublishExchangeDeclareStrategy();
 
-            publishExchangeStrategy.DeclareExchange(bus, typeof(MessageWithMultipleInterfaces), ExchangeType.Topic);
+        //    publishExchangeStrategy.DeclareExchange(bus, typeof(MessageWithMultipleInterfaces), ExchangeType.Topic);
 
-            Assert.That(exchanges, Has.Count.EqualTo(3), "Two exchanges should have been created");
-            Assert.That(exchanges[0].Name, Is.EqualTo("MessageWithMultipleInterfaces"), "Concrete message exchange should been created first");
+        //    Assert.That(exchanges, Has.Count.EqualTo(3), "Two exchanges should have been created");
+        //    Assert.That(exchanges[0].Name, Is.EqualTo("MessageWithMultipleInterfaces"), "Concrete message exchange should been created first");
 
-            Assert.That(exchanges.Count(m => m.Name == "IMessageInterfaceOne"), Is.EqualTo(1), "One IMessageInterfaceOne exchange should be created");
-            Assert.That(exchanges.Count(m => m.Name == "IMessageInterfaceTwo"), Is.EqualTo(1), "One IMessageInterfaceTwo exchange should be created");
-            Assert.That(exchanges[0].BoundTo, Is.Null, "Superseded message exchange should route messages anywhere");
-            Assert.That(exchanges[1].BoundTo, Is.EqualTo(exchanges[0]), "Superseding message exchange should route message to superseded exchange");
-            Assert.That(exchanges[2].BoundTo, Is.EqualTo(exchanges[0]), "Superseding message exchange should route message to superseded exchange");
-        }
+        //    Assert.That(exchanges.Count(m => m.Name == "IMessageInterfaceOne"), Is.EqualTo(1), "One IMessageInterfaceOne exchange should be created");
+        //    Assert.That(exchanges.Count(m => m.Name == "IMessageInterfaceTwo"), Is.EqualTo(1), "One IMessageInterfaceTwo exchange should be created");
+        //    Assert.That(exchanges[0].BoundTo, Is.Null, "Superseded message exchange should route messages anywhere");
+        //    Assert.That(exchanges[1].BoundTo, Is.EqualTo(exchanges[0]), "Superseding message exchange should route message to superseded exchange");
+        //    Assert.That(exchanges[2].BoundTo, Is.EqualTo(exchanges[0]), "Superseding message exchange should route message to superseded exchange");
+        //}
     }
 }
