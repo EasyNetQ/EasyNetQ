7ba8a32a67de75dc8da6e6476c21a55ec4b8271e
yury.pliner <yury.pliner@gmail.com>
2018-04-24 23:42:59 +0500

    Remove sync API from PublishExchangeDeclareStrategy

--- Source/EasyNetQ/Producer/IPublishExchangeDeclareStrategy.cs
+++ Source/EasyNetQ/Producer/IPublishExchangeDeclareStrategy.cs
@@ -7,7 +7,7 @@ namespace EasyNetQ.Producer
     public interface IPublishExchangeDeclareStrategy
     {
         IExchange DeclareExchange(string exchangeName, string exchangeType);
-        IExchange DeclareExchange(Type messageType, string exchangeType);        
+        IExchange DeclareExchange(Type messageType, string exchangeType);
         Task<IExchange> DeclareExchangeAsync(string exchangeName, string exchangeType);
         Task<IExchange> DeclareExchangeAsync(Type messageType, string exchangeType);
     }
---
+++ Source/EasyNetQ/Producer/PublishExchangeDeclareStrategyExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using EasyNetQ.Topology;
+
+namespace EasyNetQ.Producer
+{
+    public static class PublishExchangeDeclareStrategyExtensions
+    {
+        public static IExchange DeclareExchange(
+            this IPublishExchangeDeclareStrategy strategy, 
+            IAdvancedBus advancedBus,
+            string exchangeName,
+            string exchangeType)
+        {
+            Preconditions.CheckNotNull(strategy, "strategy");
+            
+            return strategy.DeclareExchangeAsync(advancedBus, exchangeName, exchangeType)
+                .GetAwaiter()
+                .GetResult();
+        }
+        
+        public static IExchange DeclareExchange(
+            this IPublishExchangeDeclareStrategy strategy, 
+            IAdvancedBus advancedBus,
+            Type messageType,
+            string exchangeType)
+        {
+            Preconditions.CheckNotNull(strategy, "strategy");
+            
+            return strategy.DeclareExchangeAsync(advancedBus, messageType, exchangeType)
+                .GetAwaiter()
+                .GetResult();
+        }
+    }
+}
