67a1c860ddebd5b073a7b5c4df0c8314873c7f0d
Michael Denny <micdenny@gmail.com>
2019-03-19 09:44:38 +0100

    added comments on AwaitableDisposable to clear out the purpose of that structure [skip ci] (#922)

--- Source/EasyNetQ/Internals/AwaitableDisposable.cs
+++ Source/EasyNetQ/Internals/AwaitableDisposable.cs
@@ -4,30 +4,56 @@ using System.Threading.Tasks;
 
 namespace EasyNetQ.Internals
 {
+    /// <summary>
+    /// An awaitable wrapper around a task whose result is disposable. The wrapper is not disposable,
+    /// so this prevents usage errors like "using (MyAsync())" when the appropriate usage should be "using (await MyAsync())".
+    /// </summary>
+    /// <typeparam name="T">The type of the result of the underlying task.</typeparam>
     public struct AwaitableDisposable<T> where T : IDisposable
     {
+        /// <summary>
+        /// The underlying task.
+        /// </summary>
         private readonly Task<T> task;
 
+        /// <summary>
+        /// Initializes a new awaitable wrapper around the specified task.
+        /// </summary>
+        /// <param name="task">The underlying task to wrap. This may not be <c>null</c>.</param>
         public AwaitableDisposable(Task<T> task)
         {
             this.task = task ?? throw new ArgumentNullException(nameof(task));
         }
 
+        /// <summary>
+        /// Returns the underlying task.
+        /// </summary>
         public Task<T> AsTask()
         {
             return task;
         }
 
+        /// <summary>
+        /// Implicit conversion to the underlying task.
+        /// </summary>
+        /// <param name="source">The awaitable wrapper.</param>
         public static implicit operator Task<T>(AwaitableDisposable<T> source)
         {
             return source.AsTask();
         }
 
+        /// <summary>
+        /// Infrastructure. Returns the task awaiter for the underlying task.
+        /// </summary>
         public TaskAwaiter<T> GetAwaiter()
         {
             return task.GetAwaiter();
         }
 
+        /// <summary>
+        /// Infrastructure. Returns a configured task awaiter for the underlying task.
+        /// </summary>
+        /// <param name="continueOnCapturedContext">Whether to attempt to marshal the continuation back to the captured context.</param>
         public ConfiguredTaskAwaitable<T> ConfigureAwait(bool continueOnCapturedContext)
         {
             return task.ConfigureAwait(continueOnCapturedContext);
