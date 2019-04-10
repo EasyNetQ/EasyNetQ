bdc0d26eba98eec3075027f63edd43fd491512d4
Yury Pliner <yury.pliner@gmail.com>
2019-04-10 09:32:10 +0500

    Drop strange test of Microsoft DI

--- Source/EasyNetQ.DI.Tests/MicrosoftDependencyScopeTests.cs
+++ Source/EasyNetQ.DI.Tests/MicrosoftDependencyScopeTests.cs
@@ -62,24 +62,6 @@ namespace EasyNetQ.DI.Tests
 
             Assert.NotSame(service1, service2);
         }
-
-        [Fact]
-        public void CreateScope_TransientServiceInSameScope_ShouldBeSingleton()
-        {
-            var services = new ServiceCollection();
-            services.AddScoped<IService, Service>();
-            var resolver = services.BuildServiceProvider().GetService<IServiceResolver>();
-
-            IService service1;
-            IService service2;
-            using (var scope = resolver.CreateScope())
-            {
-                service1 = scope.Resolve<IService>();
-                service2 = scope.Resolve<IService>();
-            }
-
-            Assert.Same(service1, service2);
-        }
     }
 }
 
