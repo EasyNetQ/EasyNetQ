74f3abe5706e890adca79e9b5bb9fb823ff1c5fe
Ivan Maximov <sungam3r@yandex.ru>
2019-05-17 01:15:44 +0300

    remove _ from names

--- Source/EasyNetQ/DefaultSerializer.cs
+++ Source/EasyNetQ/DefaultSerializer.cs
@@ -5,15 +5,15 @@ namespace EasyNetQ
 {
     internal sealed class DefaultSerializer : ISerializer
     {
-        private static readonly Type _jsonBackwardCompatibilitySerializerType;
-        private readonly ISerializer _jsonBackwardCompatibilitySerializer;
+        private static readonly Type jsonBackwardCompatibilitySerializerType;
+        private readonly ISerializer jsonBackwardCompatibilitySerializer;
 
         static DefaultSerializer()
         {
             try
             {
                 var asm = Assembly.Load(new AssemblyName("EasyNetQ.Serialization.NewtonsoftJson"));
-                _jsonBackwardCompatibilitySerializerType = asm.GetType("EasyNetQ.Serialization.NewtonsoftJson.JsonSerializer", throwOnError: true);
+                jsonBackwardCompatibilitySerializerType = asm.GetType("EasyNetQ.Serialization.NewtonsoftJson.JsonSerializer", throwOnError: true);
             }
             catch (Exception)
             {
@@ -23,7 +23,7 @@ namespace EasyNetQ
 
         public DefaultSerializer()
         {
-            if (_jsonBackwardCompatibilitySerializerType == null)
+            if (jsonBackwardCompatibilitySerializerType == null)
                 throw new InvalidOperationException(@"DefaultSerializer can not find EasyNetQ.Serialization.NewtonsoftJson.dll which is used by default to serialize messages (for backward compatibility).
 To solve this issue:
 1. Use EasyNetQ package, not EasyNetQ.Core. EasyNetQ package contains both EasyNetQ.Core and EasyNetQ.Serialization.NewtonsoftJson.
@@ -33,17 +33,17 @@ To solve this issue:
    RabbitHutch.CreateBus(""host = localhost"", r => r.Register<ISerializer, MySerializer>());
 ");
 
-            _jsonBackwardCompatibilitySerializer = (ISerializer)Activator.CreateInstance(_jsonBackwardCompatibilitySerializerType);
+            jsonBackwardCompatibilitySerializer = (ISerializer)Activator.CreateInstance(jsonBackwardCompatibilitySerializerType);
         }
 
         public byte[] MessageToBytes(Type messageType, object message)
         {
-            return _jsonBackwardCompatibilitySerializer.MessageToBytes(messageType, message);
+            return jsonBackwardCompatibilitySerializer.MessageToBytes(messageType, message);
         }
 
         public object BytesToMessage(Type messageType, byte[] bytes)
         {
-            return _jsonBackwardCompatibilitySerializer.BytesToMessage(messageType, bytes);
+            return jsonBackwardCompatibilitySerializer.BytesToMessage(messageType, bytes);
         }
     }
 }
