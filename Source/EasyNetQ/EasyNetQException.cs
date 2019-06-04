9ff1bcd07c01237af4432a0b334207df5bf3433b
Ivan Maximov <sungam3r@yandex.ru>
2019-06-04 08:25:57 +0300

    Make EasyNetQ exceptions really serializable
    Fixes #705 (#953)

--- Source/EasyNetQ/EasyNetQException.cs
+++ Source/EasyNetQ/EasyNetQException.cs
@@ -1,20 +1,25 @@
 ﻿using System;
+using System.Runtime.Serialization;
 
 namespace EasyNetQ
 {
+    [Serializable]
     public class EasyNetQException : Exception
     {
-        public EasyNetQException() {}
-        public EasyNetQException(string message) : base(message) {}
-        public EasyNetQException(string format, params string[] args) : base(string.Format(format, args)) {}
-        public EasyNetQException(string message, Exception inner) : base(message, inner) {}
+        public EasyNetQException() { }
+        public EasyNetQException(string message) : base(message) { }
+        public EasyNetQException(string format, params object[] args) : base(string.Format(format, args)) { }
+        public EasyNetQException(string message, Exception inner) : base(message, inner) { }
+        protected EasyNetQException(SerializationInfo info, StreamingContext context) : base(info, context) { }
     }
 
+    [Serializable]
     public class EasyNetQResponderException : EasyNetQException
     {
         public EasyNetQResponderException() { }
         public EasyNetQResponderException(string message) : base(message) { }
-        public EasyNetQResponderException(string format, params string[] args) : base(format, args) { }
+        public EasyNetQResponderException(string format, params object[] args) : base(format, args) { }
         public EasyNetQResponderException(string message, Exception inner) : base(message, inner) { }
+        protected EasyNetQResponderException(SerializationInfo info, StreamingContext context) : base(info, context) { }
     }
 }
