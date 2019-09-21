e5fb466bd33aecfe4f1b2384065ccea41371b3ec
ZK-Andy <51398532+ZK-Andy@users.noreply.github.com>
2019-09-21 15:13:59 +0800

    Fix  #978
    communicate between .Net Core and .Net Framework application  
    
    This is my first GitHub submission. If anything is wrong, please forgive me.

--- Source/EasyNetQ/LegacyTypeNameSerializer.cs
+++ Source/EasyNetQ/LegacyTypeNameSerializer.cs
@@ -20,6 +20,10 @@ namespace EasyNetQ
                     throw new EasyNetQException("type name {0}, is not a valid EasyNetQ type name. Expected Type:Assembly", t);
                 }
                 var type = Type.GetType(nameParts[0] + ", " + nameParts[1]);
+                if (type == null && typeof(string).FullName == nameParts[0])
+                {
+                    type = Type.GetType(nameParts[0]);
+                }
                 if (type == null)
                 {
                     throw new EasyNetQException("Cannot find type {0}", t);
