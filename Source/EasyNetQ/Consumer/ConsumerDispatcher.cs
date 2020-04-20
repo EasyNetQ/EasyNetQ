5b7c93741c4879e7742441750236462a22ad8fbe
René Rotvig Jensen <renerotvig@msn.com>
2020-04-20 11:22:01 +0200

    Fix deadlock when disposing internal consumers

--- Source/EasyNetQ/Consumer/ConsumerDispatcher.cs
+++ Source/EasyNetQ/Consumer/ConsumerDispatcher.cs
@@ -8,53 +8,73 @@ namespace EasyNetQ.Consumer
     public class ConsumerDispatcher : IConsumerDispatcher
     {
         private readonly ILog logger = LogProvider.For<ConsumerDispatcher>();
-        private readonly BlockingCollection<Action> queue;
-        private bool disposed;
+        private readonly AutoResetEvent autoResetEvent = new AutoResetEvent(false);
+        private readonly ConcurrentQueue<Action> durableActions = new ConcurrentQueue<Action>();
+        private readonly ConcurrentQueue<Action> transientActions = new ConcurrentQueue<Action>();
 
         public ConsumerDispatcher(ConnectionConfiguration configuration)
         {
             Preconditions.CheckNotNull(configuration, "configuration");
 
-            queue = new BlockingCollection<Action>();
-
             var thread = new Thread(_ =>
             {
-                while (!disposed && queue.TryTake(out var action, -1))
-                {
+                while(!IsDone())
                     try
                     {
-                        action();
+                        if (durableActions.TryDequeue(out var action) || transientActions.TryDequeue(out action))
+                            action();
+                        else
+                            autoResetEvent.WaitOne();
                     }
                     catch (Exception exception)
                     {
                         logger.ErrorException(string.Empty, exception);
                     }
-                }
             }) {Name = "EasyNetQ consumer dispatch thread", IsBackground = configuration.UseBackgroundThreads};
             thread.Start();
         }
 
+        public bool IsDisposed { get; private set; }
         public void QueueAction(Action action)
         {
+            QueueAction(action, false);
+        }
+
+        public void QueueAction(Action action, bool surviveDisconnect)
+        {
             Preconditions.CheckNotNull(action, "action");
-            queue.Add(action);
+            if (IsDisposed)
+                throw new ObjectDisposedException(nameof(ConsumerDispatcher));
+
+            if (surviveDisconnect)
+            {
+                durableActions.Enqueue(action);
+            }
+            else
+            {
+                transientActions.Enqueue(action);
+            }
+            autoResetEvent.Set();
         }
 
         public void OnDisconnected()
         {
             // throw away any queued actions. RabbitMQ will redeliver any in-flight
             // messages that have not been acked when the connection is lost.
-            while (queue.TryTake(out _))
+            while (transientActions.TryDequeue(out _))
             {
             }
         }
 
         public void Dispose()
         {
-            queue.CompleteAdding();
-            disposed = true;
+            IsDisposed = true;
+            autoResetEvent.Set();
         }
 
-        public bool IsDisposed => disposed;
+        private bool IsDone()
+        {
+            return IsDisposed && durableActions.IsEmpty && transientActions.IsEmpty;
+        }
     }
 }
--- Source/EasyNetQ/Consumer/InternalConsumer.cs
+++ Source/EasyNetQ/Consumer/InternalConsumer.cs
@@ -3,11 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using EasyNetQ.Events;
 using EasyNetQ.Logging;
 using EasyNetQ.Topology;
 using RabbitMQ.Client;
-using RabbitMQ.Client.Events;
 
 namespace EasyNetQ.Consumer
 {
@@ -27,129 +25,6 @@ namespace EasyNetQ.Consumer
         );
 
         event Action<IInternalConsumer> Cancelled;
-    }
-
-    public class BasicConsumer : IBasicConsumer, IDisposable
-    {
-        private readonly ILog logger = LogProvider.For<BasicConsumer>();
-        private readonly Action<BasicConsumer> cancelled;
-        private readonly IConsumerDispatcher consumerDispatcher;
-        private readonly IEventBus eventBus;
-        private readonly IHandlerRunner handlerRunner;
-
-        public BasicConsumer(Action<BasicConsumer> cancelled, IConsumerDispatcher consumerDispatcher, IQueue queue, IEventBus eventBus, IHandlerRunner handlerRunner, Func<byte[], MessageProperties, MessageReceivedInfo, Task> onMessage, IModel model)
-        {
-            Preconditions.CheckNotNull(onMessage, "onMessage");
-
-            Queue = queue;
-            OnMessage = onMessage;
-            this.cancelled = cancelled;
-            this.consumerDispatcher = consumerDispatcher;
-            this.eventBus = eventBus;
-            this.handlerRunner = handlerRunner;
-            Model = model;
-        }
-
-        public Func<byte[], MessageProperties, MessageReceivedInfo, Task> OnMessage { get; }
-        public IQueue Queue { get; }
-        public string ConsumerTag { get; private set; }
-
-        public void HandleBasicConsumeOk(string consumerTag)
-        {
-            ConsumerTag = consumerTag;
-        }
-
-        public void HandleBasicDeliver(string consumerTag, ulong deliveryTag, bool redelivered, string exchange, string routingKey,
-            IBasicProperties properties, ReadOnlyMemory<byte> body)
-        {
-
-            if (logger.IsDebugEnabled())
-            {
-                logger.DebugFormat("Message delivered to consumer {consumerTag} with deliveryTag {deliveryTag}", consumerTag, deliveryTag);
-            }
-
-            if (disposed)
-            {
-                // this message's consumer has stopped, so just return
-                logger.InfoFormat(
-                    "Consumer with consumerTag {consumerTag} on queue {queue} has stopped running. Ignoring message",
-                    ConsumerTag,
-                    Queue.Name
-                );
-
-                return;
-            }
-
-            var bodyBytes = body.ToArray();
-            var messageReceivedInfo = new MessageReceivedInfo(consumerTag, deliveryTag, redelivered, exchange, routingKey, Queue.Name);
-            var messageProperties = new MessageProperties(properties);
-            var context = new ConsumerExecutionContext(OnMessage, messageReceivedInfo, messageProperties, bodyBytes);
-
-            eventBus.Publish(new DeliveredMessageEvent(messageReceivedInfo, messageProperties, bodyBytes));
-            handlerRunner.InvokeUserMessageHandlerAsync(context)
-                .ContinueWith(async x =>
-                    {
-                        var ackStrategy = await x.ConfigureAwait(false);
-                        consumerDispatcher.QueueAction(() =>
-                        {
-                            var ackResult = ackStrategy(Model, deliveryTag);
-                            eventBus.Publish(new AckEvent(messageReceivedInfo, messageProperties, bodyBytes, ackResult));
-                        });
-                    },
-                    TaskContinuationOptions.ExecuteSynchronously
-                );
-        }
-
-        /// <summary>
-        /// Cancel means that an external signal has requested that this consumer should
-        /// be cancelled. This is _not_ the same as when an internal consumer stops consuming
-        /// because it has lost its channel/connection.
-        /// </summary>
-        private void Cancel()
-        {
-            // copy to temp variable to be thread safe.
-            var localCancelled = cancelled;
-            localCancelled?.Invoke(this);
-
-            var consumerCancelled = ConsumerCancelled;
-            consumerCancelled?.Invoke(this, new ConsumerEventArgs(new [] {ConsumerTag}));
-        }
-
-        public void HandleBasicCancelOk(string consumerTag)
-        {
-            Cancel();
-        }
-
-        public void HandleBasicCancel(string consumerTag)
-        {
-            Cancel();
-            logger.InfoFormat(
-                "Consumer with consumerTag {consumerTag} has cancelled",
-                consumerTag
-            );
-        }
-
-        public void HandleModelShutdown(object model, ShutdownEventArgs reason)
-        {
-            logger.InfoFormat(
-                "Consumer with consumerTag {consumerTag} on queue {queue} has shutdown with reason {reason}",
-                ConsumerTag,
-                Queue.Name,
-                reason
-            );
-        }
-
-        public IModel Model { get; }
-        public event EventHandler<ConsumerEventArgs> ConsumerCancelled;
-
-        private bool disposed;
-        public void Dispose()
-        {
-            if (disposed) return;
-            disposed = true;
-
-            eventBus.Publish(new ConsumerModelDisposedEvent(ConsumerTag));
-        }
     }
 
     public class InternalConsumer : IInternalConsumer
@@ -332,21 +207,19 @@ namespace EasyNetQ.Consumer
         public void Dispose()
         {
             if (disposed) return;
+
             disposed = true;
 
             var model = Model;
             if (model != null)
             {
                 // Queued because we may be on the RabbitMQ.Client dispatch thread.
-                var disposedEvent = new AutoResetEvent(false);
                 consumerDispatcher.QueueAction(() =>
-                    {
-                        foreach (var c in basicConsumers)
-                            c.Dispose();
-                        model.Dispose();
-                        disposedEvent.Set();
-                    });
-                disposedEvent.WaitOne();
+                {
+                    foreach (var c in basicConsumers)
+                        c.Dispose();
+                    model.Dispose();
+                }, true);
             }
         }
     }
