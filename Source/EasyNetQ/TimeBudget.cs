6f1a4a3c5ae1837215bdd5088a2f6e59fd87191a
Yury Pliner <yury.pliner@gmail.com>
2020-04-24 10:32:09 +0500

    Drop infinite timeout from TimeBudget

--- Source/EasyNetQ/TimeBudget.cs
+++ Source/EasyNetQ/TimeBudget.cs
@@ -5,9 +5,9 @@ using System.Threading;
 namespace EasyNetQ
 {
     public sealed class TimeBudget
-    {   
+    {
         private static readonly TimeSpan Precision = TimeSpan.FromMilliseconds(1);
-        
+
         private readonly TimeSpan budget;
         private readonly Stopwatch watch;
 
@@ -17,11 +17,6 @@ namespace EasyNetQ
             this.budget = budget;
         }
 
-        public static TimeBudget Infinite()
-        {
-            return new TimeBudget(Stopwatch.StartNew(), Timeout.InfiniteTimeSpan);
-        }
-
         public static TimeBudget Start(TimeSpan budget)
         {
             return new TimeBudget(Stopwatch.StartNew(), budget);
@@ -29,22 +24,12 @@ namespace EasyNetQ
 
         public bool IsExpired()
         {
-            if (budget == Timeout.InfiniteTimeSpan)
-            {
-                return false;
-            }
-
             var remaining = budget - watch.Elapsed;
             return remaining < Precision;
         }
 
         public static implicit operator TimeSpan(TimeBudget source)
         {
-            if (source.budget == Timeout.InfiniteTimeSpan)
-            {
-                return Timeout.InfiniteTimeSpan;
-            }
-
             var remaining = source.budget - source.watch.Elapsed;
             return remaining < Precision ? TimeSpan.Zero : remaining;
         }
