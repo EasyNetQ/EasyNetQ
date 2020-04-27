8e0cb5cba1932b1f89a363ad5dcd768afe0eb0a0
Ivan Maximov <sungam3r@yandex.ru>
2020-04-27 15:10:45 +0300

    add durable and expires properties to responder configuration

--- Source/EasyNetQ/Producer/IResponderConfiguration.cs
+++ Source/EasyNetQ/Producer/IResponderConfiguration.cs
@@ -3,7 +3,12 @@
     public interface IResponderConfiguration
     {
         IResponderConfiguration WithPrefetchCount(ushort prefetchCount);
+
         IResponderConfiguration WithQueueName(string queueName);
+
+        IResponderConfiguration WithDurable(bool durable = true);
+
+        IResponderConfiguration WithExpires(int? expires);
     }
 
     public class ResponderConfiguration : IResponderConfiguration
@@ -14,7 +19,18 @@
         }
 
         public ushort PrefetchCount { get; private set; }
+
         public string QueueName { get; private set; }
+
+        /// <summary>
+        /// Durable queues remain active when a server restarts.
+        /// </summary>
+        public bool Durable { get; private set; } = true;
+
+        /// <summary>
+        /// Determines how long (in milliseconds) a queue can remain unused before it is automatically deleted by the server.
+        /// </summary>
+        public int? Expires { get; private set; }
 
         public IResponderConfiguration WithPrefetchCount(ushort prefetchCount)
         {
@@ -25,6 +41,18 @@
         public IResponderConfiguration WithQueueName(string queueName)
         {
             QueueName = queueName;
+            return this;
+        }
+
+        public IResponderConfiguration WithDurable(bool durable = true)
+        {
+            Durable = durable;
+            return this;
+        }
+
+        public IResponderConfiguration WithExpires(int? expires)
+        {
+            Expires = expires;
             return this;
         }
     }
