8f662fb5e75b6dcbd32f5c5e0bf1ad3787f78d15
Yury Pliner <yury.pliner@gmail.com>
2020-10-06 00:11:14 +0500

    Trigger CI

--- Source/EasyNetQ/PersistentConnectionFactory.cs
+++ Source/EasyNetQ/PersistentConnectionFactory.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace EasyNetQ
+﻿namespace EasyNetQ
 {
     public class PersistentConnectionFactory : IPersistentConnectionFactory
     {
