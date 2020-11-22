7858cbb8ed298c48bc3c7bc386b7bf85e4b2aa65
Yury Pliner <yury.pliner@gmail.com>
2020-11-22 23:07:43 +0500

    Fix tests

--- Source/EasyNetQ.Tests/ConsumeTests/When_a_nack_received_from_the_message_handler.cs
+++ Source/EasyNetQ.Tests/ConsumeTests/When_a_nack_received_from_the_message_handler.cs
@@ -19,14 +19,6 @@ namespace EasyNetQ.Tests.ConsumeTests
         {
             MockBuilder.Channels[0].Received().BasicNack(DeliverTag, false, true);
         }
-
-        [Fact]
-        public void Should_dispose_of_the_consumer_error_strategy_when_the_bus_is_disposed()
-        {
-            MockBuilder.Bus.Dispose();
-
-            ConsumerErrorStrategy.Received().Dispose();
-        }
     }
 }
 
