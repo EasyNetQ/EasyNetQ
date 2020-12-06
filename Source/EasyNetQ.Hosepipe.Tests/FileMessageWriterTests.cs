ff804acef4a2a6f00e5622b5a3e820cfcc94a56e
Yury Pliner <yury.pliner@gmail.com>
2020-12-06 18:47:26 +0500

    Run unit tests and integrations test for netcoreapp3 via GA (#1163)
    * First version of GA integration
    
    * Fix
    
    * Install mono
    
    * Install mono with sudo
    
    * Build with msbuild
    
    * Downgrade ubuntu version
    
    * Clone all repo because otherwise LibGit2Sharp fails
    
    * Try to fix build
    
    * Try to fix build
    
    * Try to fix build
    
    * Fix run of tests
    
    * Try to run tests
    
    * Try to run tests
    
    * Try to run integration tests
    
    * Path connection string to docker
    
    * Run other tests

--- Source/EasyNetQ.Hosepipe.Tests/FileMessageWriterTests.cs
+++ Source/EasyNetQ.Hosepipe.Tests/FileMessageWriterTests.cs
@@ -3,7 +3,6 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
-using EasyNetQ.Tests;
 using Xunit;
 
 namespace EasyNetQ.Hosepipe.Tests
@@ -12,7 +11,7 @@ namespace EasyNetQ.Hosepipe.Tests
     {
         private readonly string tempDirectory = Path.Combine(Path.GetTempPath(), @"MessageOutput");
 
-        [Fact][Explicit("Writes files to the file system")]
+        [Fact][Traits.Explicit("Writes files to the file system")]
         public void WriteSomeFiles()
         {
             var directory = new DirectoryInfo(tempDirectory);
