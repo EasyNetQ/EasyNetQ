01da83addf9e2f0c275dcd200bd30252c50c3114
Максимов Иван Алексеевич <maksimov.ial@open.ru>
2021-09-07 18:01:41 +0300

    Approval tests

---
+++ Source/EasyNetQ.Approval.Tests/ApprovalTests.cs
@@ -0,0 +1,31 @@
+using PublicApiGenerator;
+using Shouldly;
+using System;
+using Xunit;
+
+namespace EasyNetQ.Tests
+{
+    public class ApprovalTests
+    {
+        [Theory]
+        [InlineData(typeof(RabbitBus))]
+        [InlineData(typeof(DI.Autofac.AutofacAdapter))]
+        [InlineData(typeof(DI.LightInject.LightInjectAdapter))]
+        [InlineData(typeof(DI.Microsoft.ServiceCollectionAdapter))]
+        [InlineData(typeof(DI.Ninject.NinjectAdapter))]
+        [InlineData(typeof(DI.SimpleInjector.SimpleInjectorAdapter))]
+        [InlineData(typeof(DI.StructureMap.StructureMapAdapter))]
+        [InlineData(typeof(DI.Windsor.WindsorAdapter))]
+        public void Public_api_should_not_be_changed_unintentionally(Type type)
+        {
+            string publicApi = type?.Assembly.GeneratePublicApi(new ApiGeneratorOptions
+            {
+                IncludeAssemblyAttributes = false,
+                WhitelistedNamespacePrefixes = new[] { "Microsoft.Extensions.DependencyInjection" },
+                ExcludeAttributes = new[] { "System.Diagnostics.DebuggerDisplayAttribute" },
+            });
+
+            publicApi.ShouldMatchApproved(options => options.WithFilenameGenerator((_, __, fileType, fileExtension) => $"{type.Assembly.GetName().Name}.{fileType}.{fileExtension}"));
+        }
+    }
+}
