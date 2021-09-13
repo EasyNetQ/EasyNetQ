9d25124236eecc10b1d535f10b00fcb5c25d128c
Yury Pliner <yury.pliner@gmail.com>
2021-09-13 18:51:21 +0500

    Allow to connect manually (#1266)
    * Allow to connect manually
    
    * Fix formatting
    
    * Fix formatting
    
    * Fix tests
    
    * Publish ConnectionCreatedEvent after initializedConnection is set

--- Source/EasyNetQ.Tests/PersistentConnectionTests.cs
+++ Source/EasyNetQ.Tests/PersistentConnectionTests.cs
@@ -11,11 +11,42 @@ namespace EasyNetQ.Tests
     public class PersistentConnectionTests
     {
         [Fact]
+        public void Should_fail_if_connect_failed()
+        {
+            var mockBuilder = new MockBuilder();
+            mockBuilder.ConnectionFactory.CreateConnection(Arg.Any<IList<AmqpTcpEndpoint>>())
+                .Returns(_ => throw new Exception("Test"));
+
+            using var connection = new PersistentConnection(
+                new ConnectionConfiguration(), mockBuilder.ConnectionFactory, mockBuilder.EventBus
+            );
+
+            Assert.Throws<Exception>(() => connection.Connect());
+
+            connection.IsConnected.Should().BeFalse();
+            mockBuilder.ConnectionFactory.Received().CreateConnection(Arg.Any<IList<AmqpTcpEndpoint>>());
+        }
+
+        [Fact]
+        public void Should_connect()
+        {
+            var mockBuilder = new MockBuilder();
+            using var connection = new PersistentConnection(
+                new ConnectionConfiguration(), mockBuilder.ConnectionFactory, mockBuilder.EventBus
+            );
+
+            connection.Connect();
+
+            connection.IsConnected.Should().BeTrue();
+            mockBuilder.ConnectionFactory.Received(1).CreateConnection(Arg.Any<IList<AmqpTcpEndpoint>>());
+        }
+
+        [Fact]
         public void Should_be_not_connected_if_connection_not_established()
         {
             var mockBuilder = new MockBuilder();
             mockBuilder.ConnectionFactory.CreateConnection(Arg.Any<IList<AmqpTcpEndpoint>>())
-                .Returns(c => throw new Exception("Test"));
+                .Returns(_ => throw new Exception("Test"));
 
             using var connection = new PersistentConnection(
                 new ConnectionConfiguration(), mockBuilder.ConnectionFactory, mockBuilder.EventBus
