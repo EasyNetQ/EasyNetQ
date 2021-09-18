c562e467cd0e12d45b4105564ccffcebce9e08b8
Yury Pliner <yury.pliner@gmail.com>
2021-09-18 14:41:40 +0500

    Fix format

--- Source/EasyNetQ.Tests/ConsumeTests/When_a_message_is_received.cs
+++ Source/EasyNetQ.Tests/ConsumeTests/When_a_message_is_received.cs
@@ -1,4 +1,4 @@
-﻿// ReSharper disable InconsistentNaming
+// ReSharper disable InconsistentNaming
 using System;
 using System.Text;
 using System.Threading;
--- Source/EasyNetQ.Tests/ModelCleanupTests.cs
+++ Source/EasyNetQ.Tests/ModelCleanupTests.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Threading;
 using EasyNetQ.Events;
 using EasyNetQ.Tests.Mocking;
