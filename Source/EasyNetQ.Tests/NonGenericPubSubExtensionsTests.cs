82ec05895f4dcd010f9722553131de0a8087d0a7
Yury Pliner <yury.pliner@gmail.com>
2021-10-02 11:36:37 +0300

    NonGeneric subscribe methods implementation (#1285)

--- Source/EasyNetQ.Tests/NonGenericPubSubExtensionsTests.cs
+++ Source/EasyNetQ.Tests/NonGenericPubSubExtensionsTests.cs
@@ -1,6 +1,9 @@
 using System;
 using System.Threading;
 using System.Threading.Tasks;
+using EasyNetQ.Consumer;
+using EasyNetQ.Internals;
+using EasyNetQ.Topology;
 using NSubstitute;
 using Xunit;
 
@@ -8,12 +11,23 @@ namespace EasyNetQ.Tests
 {
     public class NonGenericPubSubExtensionsTests
     {
-        private readonly Action<IPublishConfiguration> configure = _ => { };
+        private readonly Action<IPublishConfiguration> publishConfigure = _ => { };
+        private readonly Action<ISubscriptionConfiguration> subscribeConfigure = _ => { };
         private readonly IPubSub pubSub;
+        private readonly AwaitableDisposable<SubscriptionResult> subscribeResult;
 
         public NonGenericPubSubExtensionsTests()
         {
             pubSub = Substitute.For<IPubSub>();
+
+            var exchange = new Exchange("test");
+            var queue = new Queue("test");
+
+            subscribeResult = new AwaitableDisposable<SubscriptionResult>(
+                Task.FromResult(
+                    new SubscriptionResult(exchange, queue, DisposableAction.Create(_ => { }, 42))
+                )
+            );
         }
 
         [Fact]
@@ -21,13 +35,13 @@ namespace EasyNetQ.Tests
         {
             var message = DateTime.UtcNow;
             var messageType = typeof(DateTime);
-            await pubSub.PublishAsync(message, messageType, configure);
+            await pubSub.PublishAsync(message, messageType, publishConfigure);
 
 #pragma warning disable 4014
             pubSub.Received()
                 .PublishAsync(
                     Arg.Is(message),
-                    Arg.Is(configure),
+                    Arg.Is(publishConfigure),
                     Arg.Any<CancellationToken>()
                 );
 #pragma warning restore 4014
@@ -39,13 +53,13 @@ namespace EasyNetQ.Tests
             var message = new Dog();
             var messageType = typeof(Dog);
 
-            await pubSub.PublishAsync(message, messageType, configure);
+            await pubSub.PublishAsync(message, messageType, publishConfigure);
 
 #pragma warning disable 4014
             pubSub.Received()
                 .PublishAsync(
                     Arg.Is(message),
-                    Arg.Is(configure),
+                    Arg.Is(publishConfigure),
                     Arg.Any<CancellationToken>()
                 );
 #pragma warning restore 4014
@@ -57,15 +71,69 @@ namespace EasyNetQ.Tests
             var message = (IAnimal)new Dog();
             var messageType = typeof(IAnimal);
 
-            await pubSub.PublishAsync(message, messageType, configure);
+            await pubSub.PublishAsync(message, messageType, publishConfigure);
 
 #pragma warning disable 4014
             pubSub.Received()
                 .PublishAsync(
                     Arg.Is(message),
-                    Arg.Is(configure),
+                    Arg.Is(publishConfigure),
                     Arg.Any<CancellationToken>()
                 );
+#pragma warning restore 4014
+        }
+
+        [Fact]
+        public async Task Should_be_able_to_subscribe()
+        {
+            var messageType = typeof(Dog);
+            pubSub.SubscribeAsync(
+                Arg.Any<string>(),
+                Arg.Any<Func<Dog, CancellationToken, Task>>(),
+                Arg.Any<Action<ISubscriptionConfiguration>>(),
+                Arg.Any<CancellationToken>()
+            ).ReturnsForAnyArgs(subscribeResult);
+            using var _ = await pubSub.SubscribeAsync("Id", messageType, (_, _, _) => Task.FromResult(AckStrategies.Ack), subscribeConfigure);
+#pragma warning disable 4014
+            pubSub.Received()
+                .SubscribeAsync(Arg.Is("Id"), Arg.Any<Func<Dog, CancellationToken, Task>>(), Arg.Is(subscribeConfigure), Arg.Any<CancellationToken>());
+#pragma warning restore 4014
+        }
+
+        [Fact]
+        public async Task Should_be_able_to_subscribe_polymorphic()
+        {
+            var messageType = typeof(IAnimal);
+            pubSub.SubscribeAsync(
+                Arg.Any<string>(),
+                Arg.Any<Func<IAnimal, CancellationToken, Task>>(),
+                Arg.Any<Action<ISubscriptionConfiguration>>(),
+                Arg.Any<CancellationToken>()
+            ).ReturnsForAnyArgs(subscribeResult);
+
+            using var _ = await pubSub.SubscribeAsync("Id", messageType, (_, _, _) => Task.FromResult(AckStrategies.Ack), subscribeConfigure);
+#pragma warning disable 4014
+            pubSub.Received()
+                .SubscribeAsync(Arg.Is("Id"), Arg.Any<Func<IAnimal, CancellationToken, Task>>(), Arg.Is(subscribeConfigure), Arg.Any<CancellationToken>());
+#pragma warning restore 4014
+        }
+
+        [Fact]
+        public async Task Should_be_able_to_subscribe_struct()
+        {
+            var messageType = typeof(DateTime);
+
+            pubSub.SubscribeAsync(
+                Arg.Any<string>(),
+                Arg.Any<Func<DateTime, CancellationToken, Task>>(),
+                Arg.Any<Action<ISubscriptionConfiguration>>(),
+                Arg.Any<CancellationToken>()
+            ).ReturnsForAnyArgs(subscribeResult);
+
+            using var _ = await pubSub.SubscribeAsync("Id", messageType, (_, _, _) => Task.FromResult(AckStrategies.Ack), subscribeConfigure);
+#pragma warning disable 4014
+            pubSub.Received()
+                .SubscribeAsync(Arg.Is("Id"), Arg.Any<Func<DateTime, CancellationToken, Task>>(), Arg.Is(subscribeConfigure), Arg.Any<CancellationToken>());
 #pragma warning restore 4014
         }
     }
