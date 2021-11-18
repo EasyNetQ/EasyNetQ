5023ec9a8157a3dceda0525f82a790c7e03bbf66
Ivan Maximov <sungam3r@yandex.ru>
2021-11-18 21:38:48 +0300

    Add IRequestConfiguration.WithHeaders (#1283)
    * Add IRequestConfiguration.WithHeaders
    
    * fix whitespace
    
    * braces

--- Source/EasyNetQ/RequestConfiguration.cs
+++ Source/EasyNetQ/RequestConfiguration.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 namespace EasyNetQ
 {
@@ -14,19 +15,29 @@ namespace EasyNetQ
         /// Sets a priority of the message
         /// </summary>
         /// <param name="priority">The priority to set</param>
+        /// <returns>Returns a reference to itself</returns>
         IRequestConfiguration WithPriority(byte priority);
 
         /// <summary>
         /// Sets an expiration of the request
         /// </summary>
-        /// <param name="expiration"></param>
+        /// <param name="expiration">The time interval to set</param>
+        /// <returns>Returns a reference to itself</returns>
         IRequestConfiguration WithExpiration(TimeSpan expiration);
 
         /// <summary>
         /// Sets the queue name to publish to
         /// </summary>
-        /// <param name="queueName">The queue name</param>
+        /// <param name="queueName">The queue name to set</param>
+        /// <returns>Returns a reference to itself</returns>
         IRequestConfiguration WithQueueName(string queueName);
+
+        /// <summary>
+        /// Sets request headers
+        /// </summary>
+        /// <param name="headers">Headers to set</param>
+        /// <returns>Returns a reference to itself</returns>
+        IRequestConfiguration WithHeaders(IDictionary<string, object> headers);
     }
 
     internal class RequestConfiguration : IRequestConfiguration
@@ -40,6 +51,7 @@ namespace EasyNetQ
         public string QueueName { get; private set; }
         public TimeSpan Expiration { get; private set; }
         public byte? Priority { get; private set; }
+        public IDictionary<string, object> Headers { get; private set; }
 
         public IRequestConfiguration WithPriority(byte priority)
         {
@@ -56,6 +68,12 @@ namespace EasyNetQ
         public IRequestConfiguration WithQueueName(string queueName)
         {
             QueueName = queueName;
+            return this;
+        }
+
+        public IRequestConfiguration WithHeaders(IDictionary<string, object> headers)
+        {
+            Headers = headers;
             return this;
         }
     }
