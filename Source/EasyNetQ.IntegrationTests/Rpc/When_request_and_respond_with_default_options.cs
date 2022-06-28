6869a1055743dc1890a4a48248e8f530bfb42b1d
Yury Pliner <yury.pliner@gmail.com>
2022-06-28 15:16:09 +0300

    Amend rpc integration test to demonstrate #1416 (#1424)
    * Amend rpc integration test to demonstrate #1416
    
    * Skip test

--- Source/EasyNetQ.IntegrationTests/Rpc/When_request_and_respond_with_default_options.cs
+++ Source/EasyNetQ.IntegrationTests/Rpc/When_request_and_respond_with_default_options.cs
@@ -42,12 +42,20 @@ public class When_request_and_respond_with_default_options : IDisposable
         }
     }
 
-    [Fact]
+    [Fact(Skip = "Broken test")]
     public async Task Should_receive_response()
     {
-        using var cts = new CancellationTokenSource(TimeSpan.FromSeconds(10));
+        using var cts = new CancellationTokenSource(TimeSpan.FromSeconds(20));
 
-        using (await bus.Rpc.RespondAsync<Request, Response>(x => new Response(x.Id), cts.Token))
+        using var _ = await bus.Rpc.RespondAsync<Request, Response>(x => new Response(x.Id), cts.Token);
+
+        {
+            var response = await bus.Rpc.RequestAsync<Request, Response>(new Request(42), cts.Token);
+            response.Should().Be(new Response(42));
+        }
+
+        await Task.Delay(TimeSpan.FromSeconds(10), cts.Token); // periodic consumer restart delay
+
         {
             var response = await bus.Rpc.RequestAsync<Request, Response>(new Request(42), cts.Token);
             response.Should().Be(new Response(42));
