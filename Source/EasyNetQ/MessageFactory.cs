0aa804ba425bb66af80bbbc7e14f9c05738243ec
Iurii Pliner <yury.pliner@gmail.com>
2022-10-07 09:20:09 +0100

    Ditch unused MessageFactory.CreateInstance overload (#1495)

--- Source/EasyNetQ/MessageFactory.cs
+++ Source/EasyNetQ/MessageFactory.cs
@@ -11,12 +11,6 @@ public static class MessageFactory
 {
     private static readonly ConcurrentDictionary<Type, Type> GenericMessageTypesMap = new();
 
-    public static IMessage CreateInstance(Type messageType, object? body)
-    {
-        var genericType = GenericMessageTypesMap.GetOrAdd(messageType, t => typeof(Message<>).MakeGenericType(t));
-        return (IMessage)Activator.CreateInstance(genericType, body)!;
-    }
-
     public static IMessage CreateInstance(Type messageType, object? body, MessageProperties properties)
     {
         var genericType = GenericMessageTypesMap.GetOrAdd(messageType, t => typeof(Message<>).MakeGenericType(t));
