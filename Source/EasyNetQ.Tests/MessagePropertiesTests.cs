459553a89906f77aabc22b8eac3c507c9a475575
Ivan Maximov <sungam3r@yandex.ru>
2023-01-14 03:11:44 +0300

    fix

--- Source/EasyNetQ.Tests/MessagePropertiesTests.cs
+++ Source/EasyNetQ.Tests/MessagePropertiesTests.cs
@@ -18,8 +18,7 @@ public class MessagePropertiesTests
     [Fact]
     public void Should_set_ConfirmationIdHeader()
     {
-        var properties = new MessageProperties();
-        properties.SetConfirmationId(12345UL);
+        var properties = new MessageProperties().SetConfirmationId(12345UL);
         properties.Headers.Should().ContainKey(MessagePropertiesExtensions.ConfirmationIdHeader);
         properties.Headers[MessagePropertiesExtensions.ConfirmationIdHeader].Should().BeOfType<byte[]>();
         properties.Headers[MessagePropertiesExtensions.ConfirmationIdHeader].Should().BeEquivalentTo(new byte[] { (byte)'1', (byte)'2', (byte)'3', (byte)'4', (byte)'5' });
