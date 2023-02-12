aa726c0657aef691a72f1614dbe4c0d8ca1bf4a9
Yury Pliner <yury.pliner@gmail.com>
2023-02-12 17:00:51 +0000

    Mention publisher confirms to resolve #1617

--- Source/EasyNetQ/AdvancedBusExtensions.cs
+++ Source/EasyNetQ/AdvancedBusExtensions.cs
@@ -456,18 +456,20 @@ public static class AdvancedBusExtensions
     ) => bus.PublishAsync(exchange.Name, routingKey, mandatory, properties, body, cancellationToken);
 
     /// <summary>
-    ///     Publish a message as a byte array
+    /// Publish a message as a byte array.
+    /// Task completes after publish has completed. If publisherConfirms=true is set in the connection string,
+    /// the task completes after an ACK is received. The task will throw on either NACK or timeout.
     /// </summary>
     /// <param name="bus">The bus instance</param>
     /// <param name="exchange">The exchange to publish to</param>
     /// <param name="routingKey">
-    ///     The routing key for the message. The routing key is used for routing messages depending on the
-    ///     exchange configuration.
+    /// The routing key for the message. The routing key is used for routing messages depending on the
+    /// exchange configuration.
     /// </param>
     /// <param name="mandatory">
-    ///     This flag tells the server how to react if the message cannot be routed to a queue.
-    ///     If this flag is true, the server will return an unroutable message with a Return method.
-    ///     If this flag is false, the server silently drops the message.
+    /// This flag tells the server how to react if the message cannot be routed to a queue.
+    /// If this flag is true, the server will return an unroutable message with a Return method.
+    /// If this flag is false, the server silently drops the message.
     /// </param>
     /// <param name="messageProperties">The message properties</param>
     /// <param name="body">The message body</param>
@@ -488,18 +490,20 @@ public static class AdvancedBusExtensions
     }
 
     /// <summary>
-    ///     Publish a message as a byte array
+    /// Publish a message as a byte array.
+    /// Task completes after publish has completed. If publisherConfirms=true is set in the connection string,
+    /// the task completes after an ACK is received. The task will throw on either NACK or timeout.
     /// </summary>
     /// <param name="bus">The bus instance</param>
     /// <param name="exchange">The exchange to publish to</param>
     /// <param name="routingKey">
-    ///     The routing key for the message. The routing key is used for routing messages depending on the
-    ///     exchange configuration.
+    /// The routing key for the message. The routing key is used for routing messages depending on the
+    /// exchange configuration.
     /// </param>
     /// <param name="mandatory">
-    ///     This flag tells the server how to react if the message cannot be routed to a queue.
-    ///     If this flag is true, the server will return an unroutable message with a Return method.
-    ///     If this flag is false, the server silently drops the message.
+    /// This flag tells the server how to react if the message cannot be routed to a queue.
+    /// If this flag is true, the server will return an unroutable message with a Return method.
+    /// If this flag is false, the server silently drops the message.
     /// </param>
     /// <param name="messageProperties">The message properties</param>
     /// <param name="body">The message body</param>
