af15e32b6ae03e942835b5dc509c62668e1232b7
Corinna John <corinna.john@adesso.de>
2023-06-19 11:11:44 +0200

    clean-up

--- Source/EasyNetQ/RpcExtensions.cs
+++ Source/EasyNetQ/RpcExtensions.cs
@@ -87,7 +87,6 @@ public static class RpcExtensions
         return rpc.RespondAsync(asyncResponder, _ => { }, cancellationToken);
     }
 
-
     /// <summary>
     ///     Set up a responder for an RPC service.
     /// </summary>
@@ -101,6 +100,22 @@ public static class RpcExtensions
         Func<TRequest, Task<TResponse>> responder,
         CancellationToken cancellationToken = default
     ) => rpc.RespondAsync<TRequest, TResponse>((r, _) => responder(r), _ => { }, cancellationToken);
+
+    /// <summary>
+    ///     Set up a responder for an RPC service.
+    /// </summary>
+    /// <typeparam name="TRequest">The request type</typeparam>
+    /// <typeparam name="TResponse">The response type</typeparam>
+    /// <param name="rpc">The rpc instance</param>
+    /// <param name="responder">A function that performs the response</param>
+    /// <param name="configure">A function that performs the configuration</param>
+    /// <param name="cancellationToken">The cancellation token</param>
+    public static Task<IDisposable> RespondAsync<TRequest, TResponse>(
+        this IRpc rpc,
+        Func<TRequest, CancellationToken, Task<TResponse>> responder,
+        Action<IResponderConfiguration> configure,
+        CancellationToken cancellationToken = default
+    ) => rpc.RespondAsync<TRequest, TResponse>((request, _, token) => responder(request, token), configure, cancellationToken);
 
     /// <summary>
     ///     Set up a responder for an RPC service.
