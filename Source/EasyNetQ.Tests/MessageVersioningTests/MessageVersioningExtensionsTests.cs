ab4def13cf536816ca03116d056081a5414f5595
Iurii Pliner <yury.pliner@gmail.com>
2024-05-29 20:30:51 +0100

    Return back EnableMessageVersioning in test

--- Source/EasyNetQ.Tests/MessageVersioningTests/MessageVersioningExtensionsTests.cs
+++ Source/EasyNetQ.Tests/MessageVersioningTests/MessageVersioningExtensionsTests.cs
@@ -1,18 +1,7 @@
 using EasyNetQ.MessageVersioning;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.DependencyInjection.Extensions;
 
 namespace EasyNetQ.Tests.MessageVersioningTests;
-
-public static class ServiceCollectionExtensions
-{
-    public static IServiceCollection TryRegister(this IServiceCollection services, Type serviceType, Type implementationType, ServiceLifetime lifetime)
-    {
-        var descriptor = new ServiceDescriptor(serviceType, implementationType, lifetime);
-        services.TryAdd(descriptor);
-        return services;
-    }
-}
 
 public class MessageVersioningExtensionsTests
 {
@@ -20,10 +9,8 @@ public class MessageVersioningExtensionsTests
     public void When_using_EnableMessageVersioning_extension_method_required_services_are_registered()
     {
         var serviceCollection = new ServiceCollection();
-        var serviceRegister = serviceCollection as IServiceCollection;
 
-        serviceRegister.TryRegister(typeof(IExchangeDeclareStrategy), typeof(VersionedExchangeDeclareStrategy), ServiceLifetime.Singleton);
-        serviceRegister.TryRegister(typeof(IMessageSerializationStrategy), typeof(VersionedMessageSerializationStrategy), ServiceLifetime.Singleton);
+        serviceCollection.EnableMessageVersioning();
 
         Assert.Contains(serviceCollection, descriptor =>
             descriptor.ServiceType == typeof(IExchangeDeclareStrategy) &&
