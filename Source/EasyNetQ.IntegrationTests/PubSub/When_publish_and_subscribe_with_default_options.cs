cc522930b7c81c93db9aa595d400b10afe3358c5
Sergey Malyarov <smalyarov@baltbet.ru>
2025-12-16 08:10:21 +0300

    Format

--- Source/EasyNetQ.IntegrationTests/PubSub/When_publish_and_subscribe_with_default_options.cs
+++ Source/EasyNetQ.IntegrationTests/PubSub/When_publish_and_subscribe_with_default_options.cs
@@ -105,7 +105,7 @@ public class When_publish_and_subscribe_with_default_options : IDisposable, IAsy
         }
     }
 
-    [Fact(Skip ="Hang on")]
+    [Fact(Skip = "Hang on")]
     public async Task Should_survive_restart()
     {
         using var cts = new CancellationTokenSource(TimeSpan.FromSeconds(30));
